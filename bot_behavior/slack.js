d1ccea8c932eee67b9161bd50421b90dae5b729a
Andy Tillo <andy.tillo@gmail.com>
2023-09-15 07:37:46 -0700

    add default router

---
+++ bot_behavior/slack.js
@@ -0,0 +1,24 @@
+const { MessageFactory } = require('botbuilder');
+
+async function handleSlackMessage(context){
+  const response = await chatCompletion(context.activity.text);
+
+  // Create the reply
+  const replyActivity = MessageFactory.text(`slack_chat_path: ${response}`);
+
+  // Try to send as thread reply in Slack
+  try {
+    // Copy the conversation object from original message
+    replyActivity.conversation = context.activity.conversation;
+    
+    // Append the ID of the parent message to post our message as reply.
+    replyActivity.conversation.id += ":" + context.activity.channelData.SlackMessage.event.ts;
+    
+  } catch (error) {
+    console.error("An error occurred while trying to reply in thread", error);
+  }
+
+  await context.sendActivity(replyActivity);
+};
+
+module.exports = handleSlackMessage;
