73507424f0f08708a0aa381a6d667d6e0d1b7b4f
Andy Tillo <andy.tillo@gmail.com>
2023-09-15 16:58:45 -0700

    set to only respond to @bot

--- bot_behaviors/slack.js
+++ bot_behaviors/slack.js
@@ -12,46 +12,46 @@ function processSlackResponseMessage(assistantResponse) {
 }
 
 async function handleSlackMessage(context, assistantResponse) {
-    
-  // check if a thread_ts exists
-  let thread_ts = "";
-  if (context.activity.channelData && context.activity.channelData.SlackMessage && context.activity.channelData.SlackMessage.event) {
-      thread_ts = context.activity.channelData.SlackMessage.event.thread_ts || context.activity.channelData.SlackMessage.event.ts;
-  }
-
-  if(thread_ts === "" && !context.activity.conversation.id.includes(thread_ts)) {
-      // If thread_ts doesn't exist, it means it's the first message to bot.
-      const welcomeMessage = ":wave: You found @bot!  Let us chat.";
-      const welcomeActivity = MessageFactory.text(welcomeMessage);
-
-      await context.sendActivity(welcomeActivity);
-
-      // Now there should be thread_ts in context.activity 
-      thread_ts = context.activity.channelData.SlackMessage.event.thread_ts || context.activity.channelData.SlackMessage.event.ts;
-  }
-  
-  if (context.activity.channelId === 'slack') {
-      // process the assistant response message for Slack
-
-      let slackMessageResponse = processSlackResponseMessage(assistantResponse);
-      const replyActivity = MessageFactory.text(slackMessageResponse);
-
-      // Try to send as thread reply in Slack
-      try {
-          replyActivity.conversation = context.activity.conversation;
-          
-          // verify if thread_ts is already in the conversation id
-          if (!replyActivity.conversation.id.includes(thread_ts)) {
-              replyActivity.conversation.id += ":" + thread_ts;
-          }    
-      } catch (error) {
-          console.error("An error occured while trying to reply in thread", error);
+  if (context.activity.text && (context.activity.text.includes('@bot') || context.activity.text.includes('@atbot'))) {
+      // Check if a thread_ts exists
+      let thread_ts = "";
+      if (context.activity.channelData && context.activity.channelData.SlackMessage && context.activity.channelData.SlackMessage.event) {
+          thread_ts = context.activity.channelData.SlackMessage.event.thread_ts || context.activity.channelData.SlackMessage.event.ts;
       }
-      await context.sendActivity(replyActivity);
+
+      if(thread_ts === "" && !context.activity.conversation.id.includes(thread_ts)) {
+          // If thread_ts doesn't exist, it means it's the first message to bot.
+          const welcomeMessage = "Welcome! Let's start a new thread for our conversation.";
+          const welcomeActivity = MessageFactory.text(welcomeMessage);
+
+          await context.sendActivity(welcomeActivity);
+
+          // Now there should be thread_ts in context.activity 
+          thread_ts = context.activity.channelData.SlackMessage.event.thread_ts || context.activity.channelData.SlackMessage.event.ts;
+      }
+
+      if (context.activity.channelId === 'slack') {
+          // process the assistant response message for Slack
+          let slackMessageResponse = processSlackResponseMessage(assistantResponse);
+          const replyActivity = MessageFactory.text(slackMessageResponse);
+
+          // Try to send as thread reply in Slack
+          try {
+              replyActivity.conversation = context.activity.conversation;
+              
+              // verify if thread_ts is already in the conversation id
+              if (!replyActivity.conversation.id.includes(thread_ts)) {
+                  replyActivity.conversation.id += ":" + thread_ts;
+              }
+          } catch (error) {
+              console.error("An error occurred while trying to reply in thread", error);
+          }
+
+          await context.sendActivity(replyActivity);
+      }
   } else {
-      // for other channelIds, send the message as it was received
-      const replyActivity = MessageFactory.text(`default_router: ${assistantResponse}`);
-      await context.sendActivity(replyActivity);
+      // Log a message
+      console.log("SLACK.JS: Message is not invoking the bot, ignore for now!");
   }
 };
 
