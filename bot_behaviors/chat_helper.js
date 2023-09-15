d107dcf1583dfea27f52a87be05510a0220bd86b
Andy Tillo <andy.tillo@gmail.com>
2023-09-15 09:15:42 -0700

    chat.unshift complete

--- bot_behaviors/chat_helper.js
+++ bot_behaviors/chat_helper.js
@@ -1,13 +1,16 @@
 const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
 
-async function chatCompletion(chatMessages, roleMessage) {
-   const endpoint = process.env.OPENAI_API_BASE_URL;
-   const client = new OpenAIClient(endpoint, new AzureKeyCredential(process.env.OPENAI_API_KEY));
-
-   const deploymentId = process.env.OPENAI_API_DEPLOYMENT;
-
-   // Add system message to the conversation history
-   chatMessages.unshift({ role: "system", content: roleMessage });
+async function chatCompletion(chatTexts, roleMessage) {
+    const endpoint = process.env.OPENAI_API_BASE_URL;
+    const client = new OpenAIClient(endpoint, new AzureKeyCredential(process.env.OPENAI_API_KEY));
+ 
+    const deploymentId = process.env.OPENAI_API_DEPLOYMENT;
+ 
+    // Ensure chatMessages is an array
+    let chatMessages = Array.isArray(chatTexts) ? chatTexts : [];
+ 
+    // Add system message as the first message in the conversation
+    chatMessages.unshift({ role: "system", content: roleMessage });
 
    console.log(`Sending request to OpenAI API with the following parameters:
       Endpoint: ${endpoint}
