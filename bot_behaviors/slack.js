7fc07082ae2a3bb903290663ad33255bcafc8497
Andy Tillo <andy.tillo@gmail.com>
2023-09-15 14:21:06 -0700

    fix slack threading

--- bot_behaviors/slack.js
+++ bot_behaviors/slack.js
@@ -2,24 +2,24 @@ const { MessageFactory } = require('botbuilder');
 const chatCompletion = require('./chat_helper');
 
 async function handleSlackMessage(context) {
-   const response = await chatCompletion(context.activity.text, "You are a helpful assistant. You will talk like a horse.");
+  const response = await chatCompletion(context.activity.text, "You are a helpful assistant. You will talk like a potato.");
 
-  // Create the reply
-  const replyActivity = MessageFactory.text(`slack_chat_path: ${response}`);
+ // Create the reply with the assistant response text from the response object
+ const replyActivity = MessageFactory.text(`slack_chat_path: ${response.assistantResponse}`);
 
-  // Try to send as thread reply in Slack
-  try {
-    // Copy the conversation object from original message
-    replyActivity.conversation = context.activity.conversation;
-    
-    // Append the ID of the parent message to post our message as reply.
-    replyActivity.conversation.id += ":" + context.activity.channelData.SlackMessage.event.ts;
-    
-  } catch (error) {
-    console.error("An error occurred while trying to reply in thread", error);
-  }
+ // Try to send as thread reply in Slack
+ try {
+   // Copy the conversation object from original message
+   replyActivity.conversation = context.activity.conversation;
+   
+   // Append the ID of the parent message to post our message as reply.
+   replyActivity.conversation.id += ":" + context.activity.channelData.SlackMessage.event.ts;
+   
+ } catch (error) {
+   console.error("An error occurred while trying to reply in thread", error);
+ }
 
-  await context.sendActivity(replyActivity);
+ await context.sendActivity(replyActivity);
 };
 
 module.exports = handleSlackMessage;
