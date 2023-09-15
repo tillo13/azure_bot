d2bb9d48cdc29cd74a779aafcf059a50cb6f1410
Andy Tillo <andy.tillo@gmail.com>
2023-09-15 08:52:59 -0700

    test memory

--- bot_router.js
+++ bot_router.js
@@ -2,35 +2,66 @@ const { ActivityHandler, MessageFactory } = require('botbuilder');
 const handleSlackMessage = require('./bot_behaviors/slack');
 const chatCompletion = require('./bot_behaviors/chat_helper');
 
+// Welcomed User property name
+const WELCOMED_USER = 'welcomedUserProperty';
+const CHAT_MESSAGES = 'chatMessagesProperty';
+
 class EchoBot extends ActivityHandler {
-   constructor() {
+   constructor(userState) {
       super();
 
+      // Creates a new user property accessor.
+      this.welcomedUserProperty = userState.createProperty(WELCOMED_USER);
+      this.chatMessagesProperty = userState.createProperty(CHAT_MESSAGES);
+      this.userState = userState;
+
       this.onMessage(async (context, next) => {
-         if (context.activity.channelId === 'slack') {
-            await handleSlackMessage(context);
-         } 
-         else {
-            // This line calls the chatCompletion function with the text of the
-            // current activity and a standard roleMessage for non-Slack channels
-            //this is a single-use conversation though, we will add memory next...
-            const response = await chatCompletion(context.activity.text, "You are a helpful assistant. You will talk like a banjo.");
-            await context.sendActivity(MessageFactory.text(`default_router: ${response}`));
-         }
-         await next();
+         const didBotWelcomedUser = await this.welcomedUserProperty.get(context, false);
+         const chatMessagesUser = await this.chatMessagesProperty.get(context, []);
+
+        if (didBotWelcomedUser === false) {
+            if (context.activity.channelId === 'slack') {
+                await handleSlackMessage(context);
+            } else {
+                const response = await chatCompletion(context.activity.text, "You are a helpful assistant. You will talk like a child.");
+                await context.sendActivity(MessageFactory.text(`default_router: ${response}`));
+                await this.welcomedUserProperty.set(context, true);
+            }
+        } else {
+            if (context.activity.channelId === 'slack') {
+                await handleSlackMessage(context);
+            } else {
+                chatMessagesUser.push({role:"user", content:context.activity.text});
+                await this.chatMessagesProperty.set(context, chatMessagesUser);
+
+                const response = await chatCompletion(chatMessagesUser, "You are a helpful assistant. You will talk like a child.");
+                await context.sendActivity(MessageFactory.text(`default_router: ${response}`));
+                chatMessagesUser.push({role:"assistant", content:response});
+                await this.chatMessagesProperty.set(context, chatMessagesUser);
+            }
+        }
+        await next();
       });
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome to the at>ESS Chat bot!';
+            const welcomeText = 'Hello and welcome to the memory'd ATT-ESS Chat bot!';
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
                     await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
                 }
             }
-            // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
+    }
+
+    /**
+     * Override the ActivityHandler.run() method to save state changes after the bot logic completes.
+     */
+    async run(context) {
+        await super.run(context);
+        // Save state changes
+        await this.userState.saveChanges(context);
     }
 }
 
