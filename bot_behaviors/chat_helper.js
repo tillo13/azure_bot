0a454ca7af37b4cd612f117978f8fbe98bba679a
Andy Tillo <andy.tillo@gmail.com>
2023-09-15 12:04:15 -0700

    update async

--- bot_behaviors/chat_helper.js
+++ bot_behaviors/chat_helper.js
@@ -28,48 +28,35 @@ async function chatCompletion(chatTexts, roleMessage) {
     let chatMessages = Array.isArray(chatTexts) ? chatTexts : [];
 
     //check if requery necessary
+    let response = shouldRequery(chatMessages[chatMessages.length - 2]?.content)
+  
     if (response.requery) {
         // Remove system message from the start of the conversation
         chatMessages.shift();
-        // Add last message from assistant instead of original system message
+        // Add a generic system message
         chatMessages.unshift({ role: "system", content: "Let me check our past conversations, one moment..."});
         // Retry the request
-        result = await client.getChatCompletions(deploymentId, chatMessages, { maxTokens: 400 });
+        let result = await client.getChatCompletions(deploymentId, chatMessages, { maxTokens: 128 });
+      
+        return { 'assistantResponse': result.choices[0].message.content, 'requery': response.requery };
     }
-    }
+
     // Check if the system message has already been added
     if(chatMessages.length === 0 || (chatMessages[0] && chatMessages[0].role !== "system")){
-    // Add system message as the first message in the conversation
-    chatMessages.unshift({ role: "system", content: roleMessage });
+        // Add system message as the first message in the conversation
+        chatMessages.unshift({ role: "system", content: roleMessage });
     }
 
     console.log(`Sending request to OpenAI API with the following parameters:
-      Endpoint: ${endpoint}
-      Deployment Id: ${deploymentId}
-      Messages: ${JSON.stringify(chatMessages)}
-   `);
+                  Endpoint: ${endpoint}
+                  Deployment Id: ${deploymentId}
+                  Messages: ${JSON.stringify(chatMessages)}
+              `);
 
    try {
-      let result = await client.getChatCompletions(deploymentId, chatMessages, { maxTokens: 400 });
-   
-      let requery;  // Create a flag to record if requery is necessary
-      if(shouldRequery(result.choices[0].message.content)) {
-         requery = true;
-         // Remove system message from the start of the conversation
-         chatMessages.shift();
-         // Add system message as the first message in the conversation
-         chatMessages.unshift({ role: "system", content: roleMessage });
-         // Retry the request
-         result = await client.getChatCompletions(deploymentId, chatMessages, { maxTokens: 400 });
-      }
-      else {
-         requery = false;
-      }
-   
+      let result = await client.getChatCompletions(deploymentId, chatMessages, { maxTokens: 128 });    
       console.log(`Received response from OpenAI API: ${JSON.stringify(result)}`);  
-
       return { 'assistantResponse': result.choices[0].message.content, 'requery': response.requery };
-
    } 
    catch (error) {
       console.error("An error occurred while interacting with OpenAI API", error);
