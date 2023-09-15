f4d340f516b5c4a64a3f324136d39eaafdd04265
Andy Tillo <andy.tillo@gmail.com>
2023-09-15 09:02:50 -0700

    fix chat_helper async to account for array from openai

--- bot_behaviors/chat_helper.js
+++ bot_behaviors/chat_helper.js
@@ -1,33 +1,34 @@
 const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
 
-async function chatCompletion(chatText, roleMessage) {
-   const endpoint = process.env.OPENAI_API_BASE_URL;
-   const client = new OpenAIClient(endpoint, new AzureKeyCredential(process.env.OPENAI_API_KEY));
-
-   const deploymentId = process.env.OPENAI_API_DEPLOYMENT;
-
-   const messages = [
-     { role: "system", content: roleMessage },
-     { role: "user", content: chatText }
-   ];
-
-   console.log(`Sending request to OpenAI API with the following parameters:
-      Endpoint: ${endpoint}
-      Deployment Id: ${deploymentId}
-      Messages: ${JSON.stringify(messages)}
-   `);
-
-   try {
-      const result = await client.getChatCompletions(deploymentId, messages, { maxTokens: 128 });
-   
-      console.log(`Received response from OpenAI API: ${JSON.stringify(result)}`);
-  
-      return result.choices[0].message.content;
-   } 
-   catch (error) {
-      console.error("An error occurred while interacting with OpenAI API", error);
-      throw error;
-   }
-}
-
-module.exports = chatCompletion;
+async function chatCompletion(chatTexts, roleMessage) {
+    const endpoint = process.env.OPENAI_API_BASE_URL;
+    const client = new OpenAIClient(endpoint, new AzureKeyCredential(process.env.OPENAI_API_KEY));
+ 
+    const deploymentId = process.env.OPENAI_API_DEPLOYMENT;
+ 
+    const messages = [
+      { role: "system", content: roleMessage },
+      // Convert array of chat objects to array of content strings using map
+      ...chatTexts.map(chat => ({ role: "user", content: chat.content }))
+    ];
+ 
+    console.log(`Sending request to OpenAI API with the following parameters:
+       Endpoint: ${endpoint}
+       Deployment Id: ${deploymentId}
+       Messages: ${JSON.stringify(messages)}
+    `);
+ 
+    try {
+       const result = await client.getChatCompletions(deploymentId, messages, { maxTokens: 128 });
+ 
+       console.log(`Received response from OpenAI API: ${JSON.stringify(result)}`);
+ 
+       return result.choices[0].message.content;
+    } 
+    catch (error) {
+       console.error("An error occurred while interacting with OpenAI API", error);
+       throw error;
+    }
+ }
+ 
+ module.exports = chatCompletion;
