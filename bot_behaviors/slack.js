fbebe9d999310e3bf9b55ef45dc8df63ea92b83e
Andy Tillo <andy.tillo@gmail.com>
2023-09-15 23:08:43 -0700

    log convos better

--- bot_behaviors/slack.js
+++ bot_behaviors/slack.js
@@ -1,21 +1,16 @@
 const { MessageFactory } = require('botbuilder');
 const chatCompletion = require('./chat_helper');
-
-//for slack conversation.replies endpoint
 const https = require('https');
 
-// check if the message is indeed from slack
 function isFromSlack(context) {
   return context.activity.channelId === 'slack';
 }
 
-// Function for processing the assistant response message specific to Slack
 function processSlackResponseMessage(assistantResponse) {
     return `slack_chat_path: ${assistantResponse}`;
 }
 
-//get user data from slack conversations api
-async function logUserConversation(channel_id, thread_ts, apiToken) {
+async function logUserConversation(channel_id, thread_ts, apiToken, botId) {
   const options = {
     hostname: 'slack.com',
     path: `/api/conversations.replies?channel=${channel_id}&ts=${thread_ts}`,
@@ -32,58 +27,51 @@ async function logUserConversation(channel_id, thread_ts, apiToken) {
       res.on('data', d => {
         responsePayload += d;
       });
-
       res.on('end', () => {
-        let messages = JSON.parse(responsePayload).messages.filter(msg => msg.user !== 'bot');
+        let messages = JSON.parse(responsePayload).messages.filter(msg => msg.user !== botId);
         console.log('***EXTRAPOLATED CHRONOLOGICAL USER SUBMITS VIA CONVERSATIONS.REPLIES API FROM SLACK***');
         messages.forEach((msg, idx) => {
-          console.log(`${idx + 1}. [${msg.ts}] ${msg.text}`);
+          console.log(`${idx + 1}. [${msg.ts}] ${msg.text}\n`);
         });
         console.log('***END OF EXTRAPOLATION***');
         resolve();
       });
     });
-
     req.on('error', error => {
       console.error(error);
       reject(error);
     });
-
     req.end();
   });
 };
 
 let activeThreads = {};
 async function handleSlackMessage(context, assistantResponse) {
-  // Put the function call right before the code block where you decide how to process the Slack message
-  let thread_ts = "";
-  
-  if (context.activity.channelData && context.activity.channelData.SlackMessage && context.activity.channelData.SlackMessage.event) {
-      thread_ts = context.activity.channelData.SlackMessage.event.thread_ts || context.activity.channelData.SlackMessage.event.ts;
-  }
+    // Extract Bot ID from context
+    let botId = context.activity.channelData && context.activity.channelData.authorizations ? context.activity.channelData.authorizations[0].user_id : "";
 
-  // Let's add a check for the API token and channel_id before calling logUserConversation()
-  if(context.activity.channelData && context.activity.channelData.ApiToken && context.activity.channelData.SlackMessage && context.activity.channelData.SlackMessage.event.channel) {
-    let apiToken = context.activity.channelData.ApiToken;  
-    let channel_id = context.activity.channelData.SlackMessage.event.channel;  
-    await logUserConversation(channel_id, thread_ts, apiToken);
-  }    
-  
-  // Check if the message is part of a thread
-  let isThreadReply = thread_ts && (context.activity.channelData.SlackMessage.event.thread_ts === thread_ts);
-  
-  // If the message includes '@bot' or '@atbot', add the thread to the activeThreads 
-  if (context.activity.text && (context.activity.text.includes('@bot') || context.activity.text.includes('@atbot'))) {
-    activeThreads[thread_ts] = true;
-  }
-  
-  // If the message doesn't invoke the bot, log and return immediately without further processing
-  if (!activeThreads[thread_ts]) {
-    console.log("\n\n***SLACK.JS: SLACK_PAYLOAD_WITHOUT_CALLING_BOT --IGNORING!\n\n", context.activity.text);
-    return;
-  }
+    let thread_ts = "";
+    if (context.activity.channelData && context.activity.channelData.SlackMessage && context.activity.channelData.SlackMessage.event) {
+        thread_ts = context.activity.channelData.SlackMessage.event.thread_ts || context.activity.channelData.SlackMessage.event.ts;
+    }
 
-  if (context.activity.text && activeThreads[thread_ts]) {
+    if(context.activity.channelData && context.activity.channelData.ApiToken && context.activity.channelData.SlackMessage && context.activity.channelData.SlackMessage.event.channel) {
+        let apiToken = context.activity.channelData.ApiToken;  
+        let channel_id = context.activity.channelData.SlackMessage.event.channel;  
+        await logUserConversation(channel_id, thread_ts, apiToken, botId);
+    }
+
+    let isThreadReply = thread_ts && (context.activity.channelData.SlackMessage.event.thread_ts === thread_ts);
+    if (context.activity.text && (context.activity.text.includes('@bot') || context.activity.text.includes('@atbot'))) {
+        activeThreads[thread_ts] = true;
+    }
+
+    if (!activeThreads[thread_ts]) {
+        console.log("\n\n***SLACK.JS: SLACK_PAYLOAD_WITHOUT_CALLING_BOT --IGNORING!\n\n", context.activity.text);
+        return;
+    }
+
+    if (context.activity.text && activeThreads[thread_ts]) {
 
       if (context.activity.channelId === 'slack' && thread_ts != "") {
           // process the assistant response message for Slack
