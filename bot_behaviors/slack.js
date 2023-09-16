5d0f1ccf4f08df7fa6db96674115b041fffd663f
Andy Tillo <andy.tillo@gmail.com>
2023-09-15 19:59:27 -0700

    clean decision to post2

--- bot_behaviors/slack.js
+++ bot_behaviors/slack.js
@@ -19,22 +19,25 @@ async function handleSlackMessage(context, assistantResponse) {
       }
 
       if (context.activity.channelId === 'slack' && thread_ts != "") {
-          // Process the assistant response message for Slack
+          // process the assistant response message for Slack
           let slackMessageResponse = processSlackResponseMessage(assistantResponse);
           const replyActivity = MessageFactory.text(slackMessageResponse);
 
-          // Try to send as thread reply in Slack
-          try {
-              replyActivity.replyToId = thread_ts;
+          // try to send as thread reply in Slack
+          try {     
+              replyActivity.conversation = context.activity.conversation;
+              // verify if thread_ts is already in the conversation id
+              if (!replyActivity.conversation.id.includes(thread_ts)) {
+                  replyActivity.conversation.id += ":" + thread_ts;
+              }   
+              await context.sendActivity(replyActivity);
           } catch (error) {
-              console.error("An error occurred while trying to reply in thread", error);
+              console.error("An error occurred while trying to reply in thread: ", error);
           }
-
-          await context.sendActivity(replyActivity);
       } else if (thread_ts == "") {
           console.log("\n\n***SLACK.JS: Can't identify thread, not posting anything.***\n\n");
       } else {
-          // Log a message
+          // log a message
           console.log("\n\n***SLACK.JS: Message is not invoking the bot, ignore for now!***\n\n");
       }
   }
