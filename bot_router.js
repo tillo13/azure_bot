14f485f3100e1c1f7918eade10cc1a03b590a765
Andy Tillo <andy.tillo@gmail.com>
2023-09-17 18:16:16 -0700

    add payload to chat if letMeCheckFlag hits to openai3

--- bot_router.js
+++ bot_router.js
@@ -1,5 +1,6 @@
 const { ActivityHandler, MessageFactory } = require('botbuilder');
-const { handleSlackMessage, isFromSlack } = require('./bot_behaviors/slack');
+const { postChatHistoryToSlack, handleSlackMessage, isFromSlack } = require('./bot_behaviors/slack');
+
 const chatCompletion = require('./bot_behaviors/chat_helper');
 
 const WELCOMED_USER = 'welcomedUserProperty';
