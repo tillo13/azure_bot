27b0220283034ee1fd1c7976497bb322cf6cf90b
Andy Tillo <andy.tillo@gmail.com>
2023-09-21 14:07:38 -0700

    DEV: try to fix undefined payloads2

--- bot_router.js
+++ bot_router.js
@@ -57,7 +57,7 @@ class EchoBot extends ActivityHandler {
       console.log("\n\n***BOT_ROUTER.JS: Running_OpenAI payload after saving latest response from OpenAI:\n", chatMessagesUser);
         
         // now when chatCompletion is called the 3rd time, pass cleanedFormattedMessages in
-        await chatCompletion(chatMessagesUser, PERSONALITY_OF_BOT, cleanedFormattedMessages); // cleanedFormattedMessages is passed here
+        //await chatCompletion(chatMessagesUser, PERSONALITY_OF_BOT, cleanedFormattedMessages); // cleanedFormattedMessages is passed here
     
         await this.chatMessagesProperty.set(context, chatMessagesUser);
         console.log("\n\n***BOT_ROUTER.JS: Running_OpenAI payload after saving latest response from OpenAI:\n", chatMessagesUser);
