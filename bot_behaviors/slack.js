d3f83c46aa6282e6c7bd4b195d47c74b92e79ece
Andy Tillo <andy.tillo@gmail.com>
2023-09-22 17:11:21 -0700

    square 1

--- bot_behaviors/slack.js
+++ bot_behaviors/slack.js
@@ -1,206 +1,210 @@
- //2023sept21 242pm test GOLDEN VERSION//
+//2023sept21 242pm test GOLDEN VERSION//
 
- const { MessageFactory } = require('botbuilder');
- const chatCompletion = require('./chat_helper');
- const https = require('https');
- 
- function isFromSlack(context) {
-   return context.activity.channelId === 'slack';
- }
- 
- function processSlackResponseMessage(assistantResponse) {
-     return `slack_chat_path: ${assistantResponse}`;
- }
- 
- function getBotId(apiToken) {
-   const options = {
-     hostname: 'slack.com',
-     path: '/api/auth.test',
-     method: 'POST',
-     headers: {
-       'Content-Type': 'application/x-www-form-urlencoded',
-       'Authorization': `Bearer ${apiToken}`
-     }
-   };
- 
-   let userId = '';
- 
-   return new Promise((resolve, reject) => {
-     const req = https.request(options, res => {
-       res.setEncoding('utf8');
-       res.on('data', chunk => {
-         userId += chunk;
-       });
- 
-       res.on('end', () => {
-         userId = JSON.parse(userId).user_id; 
-         resolve(userId);
-       });
-     });
- 
-     req.on('error', (e) => {
-       console.error(`problem with request: ${e.message}`);
-       reject(e);
-     });
- 
-     req.end();
-   });
- }
- 
- // Isolate the postMessageToSlack as a tasks method in broken_slack.js and 
- // make it an independent function in the new_slack.js like this.
- 
- async function postMessageToSlack(channel_id, thread_ts, message, apiToken) {
-   console.log("\n\n***SLACK.JS: Post message to Slack.");
- 
-   const data = JSON.stringify({
-     channel: channel_id,
-     thread_ts: thread_ts,
-     text: message
-   });
- 
-   const options = {
-     hostname: 'slack.com',
-     path: '/api/chat.postMessage',
-     method: 'POST',
-     headers: {
-       'Content-Type': 'application/json; charset=utf-8',
-       'Authorization': `Bearer ${apiToken}`,
-       'Content-Length': data.length
-     }
-   };
- 
-   return new Promise((resolve, reject) => {
-     const req = https.request(options, (res) => {
-       let data = '';
- 
-       res.on('data', (chunk) => {
-         data += chunk;
-       });
- 
-       res.on('end', () => {
-         console.log("\n\n***SLACK.JS: RESPONSE message payload from Slack:\n", JSON.parse(data)); // Log the response from Slack
-         resolve(data);
-       });
-     });
- 
-     req.on('error', (error) => {
-       console.error("Failed to post message to Slack: ", error);
-       reject(error);
-     });
-     req.write(data);
-     req.end();
-   });
- }
- 
- async function postChatHistoryToSlack(channel_id, thread_ts, apiToken, botId) {
-   const options = {
-     hostname: 'slack.com',
-     path: `/api/conversations.replies?channel=${channel_id}&ts=${thread_ts}`,
-     method: 'GET',
-     headers: {
-       'Authorization': `Bearer ${apiToken}`
-     }
-   };
- 
-   let responsePayload = '';
- 
-   return new Promise((resolve, reject) => {
-     const req = https.request(options, res => {
-       res.on('data', d => {
-         responsePayload += d;
-       });
- 
-       res.on('end', () => {
-        let messages = [];
-        let responseParsed = JSON.parse(responsePayload);
-        if (responseParsed.messages) {
-          messages = responseParsed.messages.filter(msg => !msg.hasOwnProperty('bot_id'));
+const { MessageFactory } = require('botbuilder');
+const chatCompletion = require('./chat_helper');
+const https = require('https');
+
+function isFromSlack(context) {
+  return context.activity.channelId === 'slack';
+}
+
+function processSlackResponseMessage(assistantResponse) {
+    return `slack_chat_path: ${assistantResponse}`;
+}
+
+function getBotId(apiToken) {
+  const options = {
+    hostname: 'slack.com',
+    path: '/api/auth.test',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': `Bearer ${apiToken}`
+    }
+  };
+
+  let userId = '';
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, res => {
+      res.setEncoding('utf8');
+      res.on('data', chunk => {
+        userId += chunk;
+      });
+
+      res.on('end', () => {
+        userId = JSON.parse(userId).user_id; 
+        resolve(userId);
+      });
+    });
+
+    req.on('error', (e) => {
+      console.error(`problem with request: ${e.message}`);
+      reject(e);
+    });
+
+    req.end();
+  });
+}
+
+// Isolate the postMessageToSlack as a tasks method in broken_slack.js and 
+// make it an independent function in the new_slack.js like this.
+
+async function postMessageToSlack(channel_id, thread_ts, message, apiToken) {
+  console.log("\n\n***SLACK.JS: Post message to Slack.");
+
+  const data = JSON.stringify({
+    channel: channel_id,
+    thread_ts: thread_ts,
+    text: message
+  });
+
+  const options = {
+    hostname: 'slack.com',
+    path: '/api/chat.postMessage',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Authorization': `Bearer ${apiToken}`,
+      'Content-Length': data.length
+    }
+  };
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        console.log("\n\n***SLACK.JS: RESPONSE message payload from Slack:\n", JSON.parse(data)); // Log the response from Slack
+        resolve(data);
+      });
+    });
+
+    req.on('error', (error) => {
+      console.error("Failed to post message to Slack: ", error);
+      reject(error);
+    });
+    req.write(data);
+    req.end();
+  });
+}
+
+async function postChatHistoryToSlack(channel_id, thread_ts, apiToken, botId) {
+  const options = {
+    hostname: 'slack.com',
+    path: `/api/conversations.replies?channel=${channel_id}&ts=${thread_ts}`,
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${apiToken}`
+    }
+  };
+
+  let responsePayload = '';
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, res => {
+      res.on('data', d => {
+        responsePayload += d;
+      });
+
+      res.on('end', () => {
+        // Extract the messages from the response
+        let messages = JSON.parse(responsePayload).messages.filter(msg => !msg.hasOwnProperty('bot_id'));
+
+        // Format the messages
+        let formattedMessages = "\n***SLACK.JS: letMeCheckFlag invoked!\nUSER MESSAGES IN THIS THREAD**\n";
+        messages.forEach((msg, idx) => {
+          formattedMessages += `\n${idx + 1}. [${msg.ts}] ${msg.text}\n`;
+        });
+        formattedMessages += "\n***END OF USER MESSAGES***";
+        //adding these 2 lines to print to the console, regardless
+        console.log(formattedMessages); 
+
+        //clean the payload and prepare it for openai
+        let cleanedFormattedMessages;
+
+        try {
+          // Remove all stars
+          cleanedFormattedMessages = formattedMessages.replace(/\*/g, '');
+        
+          // Remove specific phrases
+          cleanedFormattedMessages = cleanedFormattedMessages
+            .replace(/SLACK.JS: letMeCheckFlag invoked!/i, '')
+            .replace(/USER MESSAGES IN THIS THREAD/i, '')
+            .replace(/END OF USER MESSAGES/i, '')
+            .replace(/\n/g, ' ') // remove newline characters
+            .trim();
+        
+          cleanedFormattedMessages = "Here is what the user said so far in this thread, with timestamps: " + cleanedFormattedMessages;
+        
+        } catch (err) {
+          console.error('Error while parsing the message: ', err);
+          cleanedFormattedMessages = "Here is what the user said so far in this thread, with timestamps: " + formattedMessages;
         }
- 
-         // Format the messages
-         let formattedMessages = "\n***SLACK.JS: letMeCheckFlag invoked!\nUSER MESSAGES IN THIS THREAD**\n";
-         messages.forEach((msg, idx) => {
-           formattedMessages += `\n${idx + 1}. [${msg.ts}] ${msg.text}\n`;
-         });
-         formattedMessages += "\n***END OF USER MESSAGES***";
-         //adding these 2 lines to print to the console, regardless
-         console.log(formattedMessages); 
- 
-         //clean the payload and prepare it for openai
-         let cleanedFormattedMessages;
- 
-         try {
-           // Remove all stars
-           cleanedFormattedMessages = formattedMessages.replace(/\*/g, '');
-         
-           // Remove specific phrases
-           cleanedFormattedMessages = cleanedFormattedMessages
-             .replace(/SLACK.JS: letMeCheckFlag invoked!/i, '')
-             .replace(/USER MESSAGES IN THIS THREAD/i, '')
-             .replace(/END OF USER MESSAGES/i, '')
-             .replace(/\n/g, ' ') // remove newline characters
-             .trim();
-         
-           cleanedFormattedMessages = "Here is what the user said so far in this thread, with timestamps: " + cleanedFormattedMessages;
-         
-         } catch (err) {
-           console.error('Error while parsing the message: ', err);
-           cleanedFormattedMessages = "Here is what the user said so far in this thread, with timestamps: " + formattedMessages;
-         }
-         
-         console.log('\n\n****SLACK.JS: cleaned payload ready for Openai: ', cleanedFormattedMessages);
-         
-         resolve(cleanedFormattedMessages);
- 
-         // Call chat.postMessage API
-         let postOptions = {
-           hostname: 'slack.com',
-           path: '/api/chat.postMessage',
-           method: 'POST',
-           headers: {
-             'Content-Type': 'application/json; charset=utf-8',
-             'Authorization': `Bearer ${apiToken}`
-           }
-         };
- 
-         let postReq = https.request(postOptions, res => {
-           res.on('end', () => {
-             console.log('\n\n****SLACK.JS: Successful postChatHistoryToSlack()');
-             // to pass cleanMessage payload resolve();
-           });
-         });
- 
-         postReq.on('error', error => {
-           console.error('\n\n****SLACK.JS: Error during postChatHistoryToSlack() post request:', error);
-           reject(error);
-         });
- 
-         postReq.write(JSON.stringify({
-           channel: channel_id,
-           text: formattedMessages,
-           thread_ts: thread_ts,
-         }));
- 
-         postReq.end();
-       });
-     });
- 
-     req.on('error', error => {
-       console.error("\n\n****SLACK.JS: Error in postChatHistoryToSlack(): ", error);
-       reject(error);
-     });
- 
-     req.end();
-   });
- };
- 
- let activeThreads = {};
+        
+        console.log('\n\n****SLACK.JS: cleaned payload ready for Openai: ', cleanedFormattedMessages);
+        
+        resolve(cleanedFormattedMessages);
+
+        // Call chat.postMessage API
+        let postOptions = {
+          hostname: 'slack.com',
+          path: '/api/chat.postMessage',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'Authorization': `Bearer ${apiToken}`
+          }
+        };
+
+        let postReq = https.request(postOptions, res => {
+          res.on('end', () => {
+            console.log('\n\n****SLACK.JS: Successful postChatHistoryToSlack()');
+            // to pass cleanMessage payload resolve();
+          });
+        });
+
+        postReq.on('error', error => {
+          console.error('\n\n****SLACK.JS: Error during postChatHistoryToSlack() post request:', error);
+          reject(error);
+        });
+
+        postReq.write(JSON.stringify({
+          channel: channel_id,
+          text: formattedMessages,
+          thread_ts: thread_ts,
+        }));
+
+        postReq.end();
+      });
+    });
+
+    req.on('error', error => {
+      console.error("\n\n****SLACK.JS: Error in postChatHistoryToSlack(): ", error);
+      reject(error);
+    });
+
+    req.end();
+  });
+};
+
+let activeThreads = {};
 async function handleSlackMessage(context, assistantResponse, letMeCheckFlag) {
   console.log('\n\n***SLACK.JS: handleSlackMessage called with assistantResponse:', assistantResponse);
   console.log('\n\n***SLACK.JS: letMeCheckFlag is:', letMeCheckFlag);
 
-  let cleanedFormattedMessages;  // Declare it here
+  let cleanedFormattedMessages; // Declare the variable here
+
+  // Extract Bot Token from context
+  let apiToken = context.activity.channelData && context.activity.channelData.ApiToken;
+
+  // Get bot id
+  let botId = await getBotId(apiToken);
+  console.log('\n\n***SLACK.JS: EXTRACTED BOTID:', botId);
 
   let thread_ts = "";
   if (context.activity.channelData && context.activity.channelData.SlackMessage && context.activity.channelData.SlackMessage.event) {
@@ -211,22 +215,50 @@ async function handleSlackMessage(context, assistantResponse, letMeCheckFlag) {
     activeThreads[thread_ts] = true;
   }
 
-  let apiToken = context.activity.channelData && context.activity.channelData.ApiToken;
-  let botId = await getBotId(apiToken);
-    
-  if (context.activity.channelData && context.activity.channelData.ApiToken && context.activity.channelData.SlackMessage && context.activity.channelData.SlackMessage.event.channel) {
-    let channel_id = context.activity.channelData.SlackMessage.event.channel;
-    cleanedFormattedMessages = await postChatHistoryToSlack(channel_id, thread_ts, apiToken, botId); 
+  if (!activeThreads[thread_ts] && !context.activity.conversation.isGroup) {
+    console.log('\n\n***SLACK.JS: SLACK_PAYLOAD_WITHOUT_CALLING_BOT -- IGNORING!  User said: ', context.activity.text);
+    return;
   }
 
-  // Process the response message
-  if (context.activity.text && activeThreads[thread_ts]) {
-    if (context.activity.channelId === 'slack' && thread_ts !== "") {
-      await postMessageToSlack(context.activity.channelData.SlackMessage.event.channel, thread_ts, assistantResponse, context.activity.channelData.ApiToken);
+  if (letMeCheckFlag) {
+    if (context.activity.channelData && context.activity.channelData.ApiToken && context.activity.channelData.SlackMessage && context.activity.channelData.SlackMessage.event.channel) {
+      let apiToken = context.activity.channelData.ApiToken;
+      let channel_id = context.activity.channelData.SlackMessage.event.channel;
+      cleanedFormattedMessages = await postChatHistoryToSlack(channel_id, thread_ts, apiToken, botId); 
+
+      //await postChatHistoryToSlack(channel_id, thread_ts, apiToken, botId);
     }
   }
 
-  return cleanedFormattedMessages;
-};
+  if (context.activity.text && activeThreads[thread_ts]) {
+    console.log('\n\n***SLACK.JS: Latest user posted message:', context.activity.text); // Always log user message in the console
+
+    if (context.activity.channelId === 'slack' && thread_ts !== "") {
+      // Process the assistant response message for Slack
+      let slackMessageResponse = processSlackResponseMessage(assistantResponse);
+      const replyActivity = MessageFactory.text(slackMessageResponse);
+
+      // Try to send as thread reply in Slack
+      try {
+        replyActivity.conversation = context.activity.conversation;
+
+        // Verify if thread_ts is already in the conversation id
+        if (!replyActivity.conversation.id.includes(thread_ts)) {
+          replyActivity.conversation.id += ':' + thread_ts;
+        }
+
+        await context.sendActivity(replyActivity);
+        return cleanedFormattedMessages;
+
+      } catch (error) {
+        console.error('\n\n***SLACK.JS: An error occurred while trying to reply in the thread:', error);
+      }
+    } else if (thread_ts === "") {
+      console.log('\n\n***SLACK.JS: Can\'t identify thread, not posting anything.***');
+    } else {
+      console.log('\n\n***SLACK.JS: Message is not invoking the bot, ignoring for now!***');
+      }
+    }
+  };
 
 module.exports = { handleSlackMessage, isFromSlack };
