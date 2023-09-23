f53afbf4e99fdff74aad22f65ac8a9dcbd2ba704
Andy Tillo <andy.tillo@gmail.com>
2023-09-22 22:15:26 -0700

    clean up chat_Helper for 2nd  branch

--- bot_behaviors/chat_helper.js
+++ bot_behaviors/chat_helper.js
@@ -55,9 +55,9 @@ async function chatCompletion(chatTexts, roleMessage, cleanedFormattedMessages) 
 console.log('\n\n****CHAT_HELPER.JS: this will be undefined if the letmecheckflag is false: ', cleanedFormattedMessages);
 
 if (!cleanedFormattedMessages || cleanedFormattedMessages.trim() === "") {
-    console.log('\n\n****CHAT_HELPER.JS: NO PAYLOAD');
+    console.log('\n\n****CHAT_HELPER.JS: letmecheckflag HIT!');
 } else {
-    console.log('\n\n**********CHAT_HELPER.JS:******** PAYLOAD HIT:\n\n ', cleanedFormattedMessages);
+    console.log('\n\n**********CHAT_HELPER.JS:******** PAYLOAD1 HIT:\n\n ', cleanedFormattedMessages);
     console.log('\n\n****CHAT_HELPER.JS: the payload we want to add to is:\n\n ', chatMessages);
 
     let lastUserMessage;
@@ -68,7 +68,7 @@ if (!cleanedFormattedMessages || cleanedFormattedMessages.trim() === "") {
       }
     }
     
-    // find the index of the latest 'let me check' from the end
+    // find the index of the latest 'let me check' from the end of the convo to replace it...
     const checkMessage = "Let me check our past conversations, one moment...";
     let lastIndex;
     for (let i = chatMessages.length - 1; i >= 0; i--) {
@@ -87,7 +87,7 @@ if (!cleanedFormattedMessages || cleanedFormattedMessages.trim() === "") {
         );
     }
 
-    console.log('\n\n****CHAT_HELPER.JS: the payload we added to is now:\n\n ', chatMessages);
+    console.log('\n\n****CHAT_HELPER.JS: the payload1 we added to is now:\n\n ', chatMessages);
 }
 
     console.log(`\n***CHAT_HELPER.JS: Sending request to OpenAI API with the following parameters:\n
@@ -107,13 +107,37 @@ if (!cleanedFormattedMessages || cleanedFormattedMessages.trim() === "") {
         if (requeryStatus) {
             letMeCheckFlag = true;  // this is set if anything from shouldRequery function is hit...
         
-            for (let i = chatMessages.length - 1; i >= 0; i--) {
-                if (chatMessages[i].role === "assistant") {
-                    chatMessages[i] = { role: "assistant", content: "Let me check our past conversations, one moment..." };
+        
+            console.log('\n\n****CHAT_HELPER.JS: letmecheckflag HIT!');
+            } else {
+                console.log('\n\n**********CHAT_HELPER.JS:******** PAYLOAD2 HIT:\n\n ', cleanedFormattedMessages);
+                console.log('\n\n****CHAT_HELPER.JS: the payload we want to add to is:\n\n ', chatMessages);
+
+                let lastUserMessage;
+                for (let i = chatMessages.length - 1; i >= 0; i--) {
+                if (chatMessages[i].role === 'user') {
+                    lastUserMessage = chatMessages[i].content;
                     break;
                 }
-            }
-        
+                }
+                
+                // find the index of the latest 'let me check' from the end of the convo to replace it...
+                const checkMessage = "Let me check our past conversations, one moment...";
+                let lastIndex;
+                for (let i = chatMessages.length - 1; i >= 0; i--) {
+                    if (chatMessages[i].content === checkMessage && chatMessages[i].role === 'assistant') {
+                        lastIndex = i;
+                        break;
+                    }
+                }
+                
+                // if check message found, remove all messages after it and add new messages
+                if (lastIndex !== undefined) {
+                    chatMessages.splice(lastIndex + 1);
+                    chatMessages.push(
+                        { role: 'assistant', content: "I could not find a suitable response to your latest message. Please respond with your conversation history to this point and I will investigate." },
+                        { role: 'user', content: `Certainly, here is what I have said so far in this thread, with timestamps: ${cleanedFormattedMessages}.  Read these messages to see if you can answer my latest question of: ${lastUserMessage}.  If you cannot find a suitable response in what I have provided, state that you are sorry but couldn not find a match and suggest a topic related to what we have discussed.` }
+                    );
             result = await client.getChatCompletions(deploymentId, chatMessages, { maxTokens: validatedTokens });
         }
         // split this into 2 lines: console.log(`\n\n\n***CHAT_HELPER.JS: Response from OpenAI API:\n ${JSON.stringify(result)}`);
