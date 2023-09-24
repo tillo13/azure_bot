c997687c2c11ba4cb13d14e3b1cd8379a07e2f14
Andy Tillo <andy.tillo@gmail.com>
2023-09-23 17:29:38 -0700

    move chathistory to slack_utils.js and others

--- bot_behaviors/slack_utils.js
+++ bot_behaviors/slack_utils.js
@@ -13,6 +13,19 @@ async function executeHttpGetRequest(options) {
   });
 }
 
+async function executeHttpPostRequest(options, data = '') {
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, res => {
+      let returnData = '';
+      res.on('data', chunk => returnData += chunk);
+      res.on('end', () => resolve(JSON.parse(returnData)));
+      res.on('error', reject);
+    });
+    req.write(data);
+    req.end();
+  });
+}
+
 async function fetchConversationHistory(channelId, thread_ts, apiToken) {
   const options = {
     hostname: 'slack.com',
@@ -25,4 +38,17 @@ async function fetchConversationHistory(channelId, thread_ts, apiToken) {
   return await executeHttpGetRequest(options);
 }
 
-module.exports = { fetchConversationHistory };
+async function getBotId(apiToken) {
+  const options = {
+    hostname: 'slack.com',
+    path: '/api/auth.test',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': `Bearer ${apiToken}`
+    }
+  };
+  const response = await executeHttpGetRequest(options);
+  return response.user_id;
+}
+
+module.exports = { fetchConversationHistory, getBotId, executeHttpGetRequest, executeHttpPostRequest };
