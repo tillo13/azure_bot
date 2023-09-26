8a7d595c70b6f5fe90a8a3dacef4712f6914fc2c
Andy Tillo <andy.tillo@gmail.com>
2023-09-25 22:29:06 -0700

    stop in bot_router if slack test back to org to see if it mixes threads3

--- bot_router.js
+++ bot_router.js
@@ -4,7 +4,6 @@ const chatCompletion = require('./bot_behaviors/chat_helper');
 
 const WELCOMED_USER = 'welcomedUserProperty';
 const CHAT_MESSAGES = 'chatMessagesProperty';
-const THREAD_TS = 'thread_ts';
 const PERSONALITY_OF_BOT = "You talk like an old cowboy. You are a helpful assistant from Teradata that always checks any past conversations within this thread before responding to any new information received.";
 
 class EchoBot extends ActivityHandler {
@@ -12,7 +11,7 @@ class EchoBot extends ActivityHandler {
         super();
         this.welcomedUserProperty = userState.createProperty(WELCOMED_USER);
         this.chatMessagesProperty = userState.createProperty(CHAT_MESSAGES);
-        this.threadproperty = userState.createProperty(THREAD_TS);
+        this.thread_ts = "";
         this.userState = userState;
 
         this.onMembersAdded(async (context, next) => {
@@ -25,54 +24,63 @@ class EchoBot extends ActivityHandler {
             }
             await next();
         });
-
         this.onMessage(async (context, next) => {
             console.log('\n\n****BOT_ROUTER.JS: onMessage triggered');
-            let chatMessagesUser = await this.chatMessagesProperty.get(context, []) || [];
+            const chatMessagesUser = await this.chatMessagesProperty.get(context, []) || [];
             chatMessagesUser.push({ role: "user", content: context.activity.text });
-            const current_thread_ts = context.activity.channelData && context.activity.channelData.SlackMessage && context.activity.channelData.SlackMessage.event ?
-                  context.activity.channelData.SlackMessage.event.thread_ts || context.activity.channelData.SlackMessage.event.ts : "";
-            const savedThread_ts = await this.threadproperty.get(context, "");
-
             if (isFromSlack(context)) {
                 console.log('\n\n****BOT_ROUTER.JS: message from Slack');
-                console.log(`\n\n****BOT_ROUTER.JS: current thread_ts: ${current_thread_ts}`);
-
-                if(savedThread_ts !== current_thread_ts) {
-                    chatMessagesUser = []; 
-                    await this.threadproperty.set(context, current_thread_ts);
-                }
-
                 const botCalled = context.activity.text.includes('@bot') || context.activity.text.includes('@atbot');
-                console.log('\n\n****BOT_ROUTER.JS: bot is specifically invoked in the message:', botCalled);
-
-                if (botCalled || savedThread_ts === current_thread_ts) {
+                console.log('\n\n****BOT_ROUTER.JS: bot is specifically invoked in the message:', botCalled);  
+                const current_thread_ts = context.activity.channelData && context.activity.channelData.SlackMessage && context.activity.channelData.SlackMessage.event ?
+                  context.activity.channelData.SlackMessage.event.thread_ts || context.activity.channelData.SlackMessage.event.ts : "";
+                
+                console.log(`\n\n****BOT_ROUTER.JS: channelId is: ${context.activity.channelId}`);
+                console.log(`\n\n****BOT_ROUTER.JS: current thread_ts: ${current_thread_ts}`);
+        
+                // Logic to check for active thread
+                let isNewThread = current_thread_ts !== this.thread_ts;
+                this.thread_ts = current_thread_ts;
+                console.log('\n\n****BOT_ROUTER.JS: thread status evaluated ', isNewThread);
+        
+                if (botCalled || !isNewThread) {
                     console.log('\n\n****BOT_ROUTER.JS: Bot is specifically invoked in the message OR this message is part of an existing thread.');
-                    let chatResponse = await chatCompletion(chatMessagesUser, PERSONALITY_OF_BOT, context.activity.channelId);
+
+                    chatMessagesUser.length = isNewThread ? 0 : chatMessagesUser.length; // clear conversation history if its a new thread
+                    const chatResponse = await chatCompletion(chatMessagesUser, PERSONALITY_OF_BOT, context.activity.channelId);
                     console.log('\n\n****BOT_ROUTER.JS: Successfully fetched and processed the chat response from OpenAI API.');
-                    chatMessagesUser = chatResponse.requery ?  [{ role: "assistant", content: chatResponse.assistantResponse}] : chatMessagesUser;
-                    chatMessagesUser.push({ role: "assistant", content: chatResponse.assistantResponse });
+
+                    chatMessagesUser.push({ role: "assistant", content: chatResponse.assistantResponse});
+                    console.log(`\n\n****BOT_ROUTER.JS: assistant responded with: ${chatResponse.assistantResponse}`);
                     const result = await handleSlackMessage(context, chatResponse.assistantResponse, chatResponse.letMeCheckFlag, chatCompletion);
                     console.log('\n\n****BOT_ROUTER.JS: Successfully processed and responded to the Slack message.');
 
-                    if(chatResponse.requery && result.isActiveThread) {
+                    const isThreadActive = result.isActiveThread;
+                    
+                    if(chatResponse.requery && isThreadActive){
                         console.log('\n\n****BOT_ROUTER.JS: The flag for a requery with OpenAI was set, and the bot is currently active within a Slack thread.');
 
                         const requeryNotice = "Let me check our past conversations, one moment...";
                         await context.sendActivity(MessageFactory.text(requeryNotice, requeryNotice));
-                        const chatResponses = await chatCompletion(chatMessagesUser, PERSONALITY_OF_BOT, context.activity.channelId, result.isActiveThread);
-                        chatMessagesUser.push({ role: "assistant", content: chatResponses.assistantResponse });
+                        chatMessagesUser.push({ role: "assistant", content: requeryNotice });
+                        let chatResponse = await chatCompletion(chatMessagesUser, PERSONALITY_OF_BOT, context.activity.channelId, isThreadActive);
+                        chatMessagesUser.push({ role: "assistant", content: chatResponse.assistantResponse });
                     }
+        
                     console.log(`\n\n****BOT_ROUTER.JS: letMeCheckFlag is: ${chatResponse.letMeCheckFlag}`);
                 } 
             } else {
                 console.log('\n\n****BOT_ROUTER.JS: The received message did not originate from Slack.');
+
                 const chatResponse = await chatCompletion(chatMessagesUser, PERSONALITY_OF_BOT, context.activity.channelId);
                 console.log(`\n\n****BOT_ROUTER.JS: assistant responded with: ${chatResponse.assistantResponse}`);
-                await context.sendActivity(MessageFactory.text(`default_router: ${chatResponse.assistantResponse}`));
+                const replyActivity = MessageFactory.text(`default_router: ${chatResponse.assistantResponse}`);
+                await context.sendActivity(replyActivity);
             }
-
+        
             await this.chatMessagesProperty.set(context, chatMessagesUser);
+            console.log(`\n\n****BOT_ROUTER.JS: chat message property set with value ${JSON.stringify(chatMessagesUser, null, 2)}\n`);
+
 
             await next();
         });
