74e37c2c97267f05fce5bb9a019da1fac9c78623
Andy Tillo <andy.tillo@gmail.com>
2023-09-27 14:55:03 -0700

    add dog path

--- bot_behaviors/special_commands.js
+++ bot_behaviors/special_commands.js
@@ -49,7 +49,7 @@ async function generateDogImage(context) {
     }, {
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+            'Authorization': `Bearer ${process.env.OPENAI_DALLE_API_KEY}`
         }
     });
 
