faff60dbedd16216b04d3ea393b37b11f976ae0d
Andy Tillo <andy.tillo@gmail.com>
2023-09-28 00:13:11 -0700

    clean up special_commands to slack2

--- bot_behaviors/special_commands.js
+++ bot_behaviors/special_commands.js
@@ -1,18 +1,18 @@
 const { MessageFactory } = require('botbuilder');
 const generateImages = require('./dalle_utils');
-
 const commands = new Proxy({
     '$hamburger': addToppings,
     '$help': contactHelp,
-    '$dalle': createDalleImages
+    '$dalle': createDalleImages,
 }, {
-    get: function (target, property) {
+    get: function(target, property) {
         if (property in target) {
             return target[property];
-        }
-        for (let key in target) {
-            if (property.startsWith(key)) {
-                return target[key];
+        } else {
+            for (let key in target) {
+                if (property.startsWith(key)) {
+                    return target[key];
+                }
             }
         }
     }
@@ -28,21 +28,19 @@ async function contactHelp(context) {
 
 async function sendMessageResponse(context, message) {
     const replyActivity = MessageFactory.text(message);
-    modifyConversationID(context, replyActivity);
-    return await context.sendActivity(replyActivity);
-}
-
-function modifyConversationID(context, replyActivity) {
+    
     try {
         replyActivity.conversation = context.activity.conversation;
         const thread_ts = context.activity.channelData?.SlackMessage?.event?.thread_ts || 
-                            context.activity.channelData?.SlackMessage?.event?.ts;
-        if (thread_ts && !replyActivity.conversation.id.includes(thread_ts)) {
+                          context.activity.channelData?.SlackMessage?.event?.ts;
+        if (!replyActivity.conversation.id.includes(thread_ts)) {
             replyActivity.conversation.id += ':' + thread_ts;
         }
     } catch (error) {
         console.error('Error occurred while trying to reply in the thread:', error);
     }
+
+    return await context.sendActivity(replyActivity);
 }
 
 async function createDalleImages(context) {
@@ -56,59 +54,61 @@ async function createDalleImages(context) {
     }
 
     if (!messageText) {
-        await sendMessageResponse(context, `You did not ask for any image in particular, so get the default of a dog! Please wait a moment...`);
-        return;
+        await sendMessageWithThread(context, `You did not ask for any image in particular, so get the default of a dog! Please wait a moment...`, thread_ts);
     }
 
     let splitMessage = messageText.split(" --");
     let prompt = splitMessage[0] || "a nice photo of a dog";
     let numImages = splitMessage[1] ? parseInt(splitMessage[1]) : 1;
-    let filenameBase = sanitizeFilename(prompt);
+    
+    // Create filename-friendly version of the prompt, limit to 15 chars, and remove consecutive underscores
+    let filenameBase = prompt.replace(/[^a-z0-9_]/gi, '_').replace(/\s+/g, '').replace(/_+/g, "_").substring(0, 15);
+    filenameBase = filenameBase !== '_' ? filenameBase.trim('_') : filenameBase;
 
     if (numImages > 10) {
-        await sendMessageResponse(context, `You've asked for more than 10 images. We are going to generate the maximum allowed of 10. Please wait...`);
+        await sendMessageWithThread(context, `You've asked for more than 10 images. We are going to generate the maximum allowed of 10. Please wait...`, thread_ts);
         numImages = 10;
     }
 
-    await sendMessageResponse(context, `You asked for "${prompt}". We are generating ${numImages} image(s) for you. Each image may take a few seconds to generate. Please wait...`);
-
+    const completionMessage = `You asked for "${prompt}". We are generating ${numImages} image(s) for you. Each image may take a few seconds to generate. Please wait...`;
+    await sendMessageWithThread(context, completionMessage, thread_ts);
+    
     for(let i=0; i<numImages; i++){
         let filename = `${filenameBase}_${(i+1).toString().padStart(2, '0')}.png`;
-        await sendMessageResponse(context, `Creating ${filename}...`);
+        await sendMessageWithThread(context, `Creating ${filename}...`, thread_ts);
+
         await context.sendActivity({ type: 'typing' });
-        await generateAndSendImages(context, prompt, 1);
+        await generateImages(prompt, 1, async (imageUrl) => {
+            const replyActivity = MessageFactory.attachment({
+                contentType: 'image/png',
+                contentUrl: imageUrl,
+            });
+        
+            if (context.activity.channelId === 'slack') {
+                replyActivity.conversation = replyActivity.conversation || context.activity.conversation;  
+                if (thread_ts && !replyActivity.conversation.id.includes(thread_ts)) {
+                    replyActivity.conversation.id += ':' + thread_ts;
+                }
+            }
+        
+            await context.sendActivity(replyActivity);
+        });
     }
 
     let endTime = new Date().getTime();
     let difference = endTime - startTime;
     let seconds = (difference / 1000).toFixed(3);
-    await sendMessageResponse(context, `We generated ${numImages} image(s) for you that took a total of ${seconds} seconds. Thank you.`);
+    await sendMessageWithThread(context, `We generated ${numImages} image(s) for you that took a total of ${seconds} seconds. Thank you.`, thread_ts);
 }
+async function sendMessageWithThread(context, message, thread_ts) {
+    const newActivity = MessageFactory.text(message);
+    newActivity.conversation = context.activity.conversation; 
 
-function sanitizeFilename(prompt) {
-    let filenameBase = prompt.replace(/[^a-z0-9_]/gi, '_').replace(/\s+/g, '').replace(/_+/g, "_").substring(0, 15);
-    return filenameBase !== '_' ? filenameBase.trim('_') : filenameBase;
-}
+    if (thread_ts && !newActivity.conversation.id.includes(thread_ts)) {
+        newActivity.conversation.id += ':' + thread_ts;
+    }
 
-async function generateAndSendImages(context, prompt, numImages) {
-    await generateImages(prompt, numImages, async (imageUrl) => {
-        const replyActivity = MessageFactory.attachment({
-            contentType: 'image/png',
-            contentUrl: imageUrl,
-        });
-
-        if (context.activity.channelId === 'slack') {
-            replyActivity.conversation = replyActivity.conversation || context.activity.conversation;  
-            let thread_ts = context.activity.channelData?.SlackMessage?.event?.thread_ts ||
-                            context.activity.channelData?.SlackMessage?.event?.ts;
-
-            if (thread_ts && !replyActivity.conversation.id.includes(thread_ts)) {
-                replyActivity.conversation.id += ':' + thread_ts;
-            }
-        }
-    
-        await context.sendActivity(replyActivity);
-    });
+    await context.sendActivity(newActivity);
 }
 
 module.exports = commands;
