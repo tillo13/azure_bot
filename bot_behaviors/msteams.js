41dac658de5fff2a836aa4b548a72577148b9b18
Andy Tillo <andy.tillo@gmail.com>
2023-09-28 14:30:45 -0700

    add msteams.js clean chat_helper.

--- bot_behaviors/msteams.js
+++ bot_behaviors/msteams.js
@@ -1,5 +1,5 @@
 const { MessageFactory } = require('botbuilder');
-const chatCompletion = require('./bot_behaviors/chat_helper');
+const chatCompletion = require('chat_helper');
 
 const PERSONALITY_OF_BOT = "You talk like an old cowboy. You are a helpful chatbot from Teradata. As a crucial aspect of your function, ensure you always reference past user and assistant prompts in the thread for the best understanding in order to respond effectively.";
 
