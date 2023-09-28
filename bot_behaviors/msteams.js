77c081baf18453e1d59a62a8de4004af2379acf3
Andy Tillo <andy.tillo@gmail.com>
2023-09-28 14:47:57 -0700

    add msteams.js log files and git_push cleanup

--- bot_behaviors/msteams.js
+++ bot_behaviors/msteams.js
@@ -1,24 +1,34 @@
 const { MessageFactory } = require('botbuilder');
 const chatCompletion = require('./chat_helper');
 
-const PERSONALITY_OF_BOT = "You talk like an old cowboy. You are a helpful chatbot from Teradata. As a crucial aspect of your function, ensure you always reference past user and assistant prompts in the thread for the best understanding in order to respond effectively.";
+const PERSONALITY_OF_BOT = "You talk like an pirate. You are a helpful chatbot from Teradata. As a crucial aspect of your function, ensure you always reference past user and assistant prompts in the thread for the best understanding in order to respond effectively.";
 
 function isFromMSTeams(context) {
     return context.activity.channelId === 'msteams';
 }
 
 async function handleTeamsMessage(context, chatMessagesUser, isFirstInteraction) {
+    console.log('\n*****MSTEAMS.JS: Preparing to handle a message from MS Teams');
+    
+    // Log the entire activity object
+    console.log('\n*****MSTEAMS.JS: Teams Activity:', JSON.stringify(context.activity, null, 2));
+
+    // Log Teams-specific data
+    console.log('\n*****MSTEAMS.JS: Teams Channel Data:', JSON.stringify(context.activity.channelData, null, 2));
      
     let assistantResponse = '';
-
     if (isFirstInteraction) {
+        console.log('*****MSTEAMS.JS: This is the first user interaction');
         assistantResponse = 'msteams_chat_path: Hello from @bot in MS Teams!';
     }
     else {
+        console.log('*****MSTEAMS.JS: This is not the first interaction. Calling OpenAI...');
         const chatResponse = await chatCompletion(chatMessagesUser, PERSONALITY_OF_BOT, context.activity.channelId);
+        console.log('*****MSTEAMS.JS: Received response from OpenAI');
         assistantResponse = `MS_Teams_Chat_Path: ${chatResponse.assistantResponse}`;
     }
 
+    console.log('*****MSTEAMS.JS: Assistant Response: ', assistantResponse);
     return assistantResponse;
 }
 
--- git_push.sh
+++ git_push.sh
@@ -7,11 +7,35 @@ then
     exit 1
 fi
 
+# Check the current working branch
+currentBranch=$(git rev-parse --abbrev-ref HEAD)
+echo "Current working branch: $currentBranch"
+
+# Check the status of 
+echo "Git status before changes are added:"
+git status
+
+# Seeing which files have changes
+echo "Changed files:"
+git diff --name-only
+
 # Add all changes to the staging area
 git add .
 
 # Commit the changes
 git commit -m "$1"
 
+# Print the most recent commit
+echo "Details of latest commit:"
+git log -1 --pretty=format:"%h%x09%an%x09%ad%x09%s"
+
 # Push the changes to the 'main' branch
 git push origin main
+
+# Check the status after push
+echo "Git status after push:"
+git status
+
+# Log of latest 5 commits
+echo "Log of last 5 commits:"
+git log --pretty=format:"%h%x09%an%x09%ad%x09%s" -5
