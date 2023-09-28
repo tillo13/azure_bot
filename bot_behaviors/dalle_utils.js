a0ca8595f1950563aaa5ba1e0cd47e01b1748582
Andy Tillo <andy.tillo@gmail.com>
2023-09-27 21:03:04 -0700

    move dog to dalle

--- bot_behaviors/dalle_utils.js
+++ bot_behaviors/dalle_utils.js
@@ -1,123 +1,52 @@
-const axios = require('axios');
-const { DefaultAzureCredential } = require('@azure/identity');
-
-const OPENAI_DALLE_API_KEY = 'https://cognitiveservices.azure.com/.default'; // Replace with your resource
+const fetch = require('node-fetch');
 const OPENAI_DALLE_BASE_URL = process.env.OPENAI_DALLE_BASE_URL;
 const OPENAI_DALLE_VERSION = process.env.OPENAI_DALLE_VERSION;
-// include the other requires for sharp, fetch, etc.
+const OPENAI_DALLE_API_KEY = process.env.OPENAI_DALLE_API_KEY;
 
-const credential = new DefaultAzureCredential();
-let openai_token = null;
+async function generateImages(prompt = 'a nice photo of a dog', numImages = 1, onImageReady) {
+    const headers = { "API-Key": OPENAI_DALLE_API_KEY, "Content-Type": "application/json"};
+    const requestBody = { prompt, size: "1024x1024", n: numImages };
+    const submitUrlPath = "/openai/images/generations:submit?api-version=";
 
-const dalleResponse = async (command_text, numImages) => {
-  await ensureOpenaiToken();
+    const response = await fetch(
+        `${OPENAI_DALLE_BASE_URL}${submitUrlPath}${OPENAI_DALLE_VERSION}`,
+        { method: "POST", headers, body: JSON.stringify(requestBody) }
+    );
+    const initJob = await response.json();
 
-  const url = `${OPENAI_DALLE_BASE_URL}/openai/images/generations:submit?api-version=${OPENAI_DALLE_VERSION}`;
-  const payload = JSON.stringify({
-      'prompt': command_text,
-      'size': '1024x1024',
-      'n': numImages,
-  });
-
-  console.log(`URL: ${url}`);
-  console.log(`Payload: ${payload}`);
-
-  const response = await axios.post(url, payload, {
-    headers: {
-      'Authorization': `Bearer ${openai_token.token}`,
-      'Content-Type': 'application/json',
-    },
-  });
-
-    console.log(`URL: ${url}`);
-    console.log(`Payload: ${payload}`);
-    
-    console.log('Response: ', response);
-    console.log('Response status: ', response.status);
-    console.log('Response headers: ', response.headers.raw());
-
-    if(!response.ok) {
-        const error = await response.text();
-        console.log('Error: ', error);
-        throw new Error("DALL·E error. Status: " + response.status + " Message: " + error);
-    } else {
-        const json = await response.json();
-        return {
-            'data': json.data,
-            'n_images': json.n_images,
-            'prompt': command_text,
-        };
+    if(!initJob.id) {
+        console.error('Error occurred while submitting a job', initJob);
+        return;
     }
-};
 
-async function ensureOpenaiToken() {
-    if (!openai_token || openai_token.expiresOnTimestamp <= new Date().getTime() + 5 * 60 * 1000) {
-        openai_token = await credential.getToken(OPENAI_DALLE_API_KEY);
-    }
-}
-const downloadAndResize = async (imageData) => {
-	const imageResponse = await fetch(imageData.url);
-	const buffer = await imageResponse.buffer();
+    const jobId = initJob.id;
+    const checkJobUrlPath = "/openai/operations/images/";
 
-	let resizedBuffer = buffer;
-	const fileSizeInMegabytes = buffer.length / (1024 * 1024);
-	if (fileSizeInMegabytes > 3) {
-		const img = sharp(buffer);
-		let factor = 0.9;
-		while (resizedBuffer.length / (1024 * 1024) > 3) {
-			const { width, height } = await img.metadata();
-			resizedBuffer = await img.resize(Math.round(width * factor), Math.round(height * factor)).toBuffer();
-			factor *= 0.9;
-		}
-	}
-	return resizedBuffer;
-}
+    for (let i = 0; i < 5; i++) {
+        await new Promise(resolve => setTimeout(resolve, 1500));
 
-const uploadToSlack = async (channelId, timestamp, resizedBuffer, fileName, imageData, prompt, n_images) => {
-	const data = new FormData();
-	data.append('file', resizedBuffer, {
-		filename: fileName,
-		contentType: 'image/png',
-	});
-	data.append('channels', channelId);
-	data.append('initial_comment', `Prompt: ${prompt}\nRequested ${n_images} images\nImage: ${imageData.url}`);
-	data.append('thread_ts', timestamp);
+        const res = await fetch(
+            `${OPENAI_DALLE_BASE_URL}${checkJobUrlPath}${jobId}?api-version=${OPENAI_DALLE_VERSION}`,
+            { method: "GET", headers }
+        );
 
-	const response = await fetch("https://slack.com/api/files.upload", {
-		method: 'POST',
-		body: data,
-		headers: {
-			'Authorization': `Bearer ${SLACK_BOT_TOKEN}`,
-		},
-	});
-	
-	if (!response.ok) {
-		throw new Error(`Failed to upload image to slack: ${response.status}`);
-	}
-}
+        const job = await res.json();
 
-const handleDalleCommand = async (channelId, timestamp, commandText, numImages) => {
-    try {
-            //are the params coming over?
-    console.log("\n\ndalle_utils.js:OPENAI_DALLE_BASE_URL:", OPENAI_DALLE_BASE_URL);
-    console.log("\n\n*dalle_utils.js:OPENAI_DALLE_VERSION:", OPENAI_DALLE_VERSION);
-        const dalleData = await dalleResponse(commandText, numImages);
-        const parsedData = dalleData.data;
-        parsedData.forEach(async (data, index) => {
-            const resizedBuffer = await downloadAndResize(data);
-            await uploadToSlack(
-                channelId,
-                timestamp,
-                resizedBuffer,
-                `dalle_${dalleData.prompt}_${index + 1}_of_${dalleData.n_images}.png`,
-                data,
-                dalleData.prompt,
-                dalleData.n_images
-            );
-        });
-    } catch(err) {
-        console.error(`Error occurred while handling DALL·E command: ${err.message}`);
+        if (job.status === "succeeded") {
+            job.result.data.forEach(imageData => {
+                const imageUrl = imageData?.url;
+                if (imageUrl) {
+                    console.log('Dall-E image generated, url:', imageUrl);
+                    onImageReady && onImageReady(imageUrl);
+                }
+            });
+            break;
+        } 
+
+        if(job.status !== 'running'){
+            console.error('Unknown job status:', job.status);
+        }
     }
 }
 
-module.exports = handleDalleCommand;
+module.exports = generateImages;
--- bot_behaviors/special_commands.js
+++ bot_behaviors/special_commands.js
@@ -1,18 +1,21 @@
-const fetch = require('node-fetch'); 
-const OPENAI_DALLE_BASE_URL = process.env.OPENAI_DALLE_BASE_URL;
-const OPENAI_DALLE_VERSION = process.env.OPENAI_DALLE_VERSION;
-const handleDalleCommand = require('./dalle_utils');
+const { MessageFactory } = require('botbuilder');
+const generateImages = require('./dalle_utils');
 
 async function createDalleImages(context) {
     const messageText = context.activity.text.replace('$dalle', '').trim();
     let splitMessage = messageText.split(" --");
-    const prompt = splitMessage[0] || "a happy dog";
-    const numImages = splitMessage[1] ? parseInt(splitMessage[1]) : 3;
-
-    await handleDalleCommand(context.activity.conversation.id, context.timestamp, prompt, numImages);
+    const prompt = splitMessage[0] || "a nice photo of a dog";
+    const numImages = splitMessage[1] ? parseInt(splitMessage[1]) : 1;
 
     const completionMessage = numImages > 1 ? `Images are on their way, might take some time.` : `Image is on its way, might take some time.`;
-    return await context.sendActivity(completionMessage);
+    await context.sendActivity(completionMessage);
+    await generateImages(prompt, numImages, async (imageUrl) => {
+        const replyActivity = MessageFactory.attachment({
+            contentType: 'image/png',
+            contentUrl: imageUrl,
+        });
+        await context.sendActivity(replyActivity);
+    });
 }
 
 async function addToppings(context) {
@@ -39,81 +42,11 @@ async function sendMessageResponse(context, message) {
 
     return await context.sendActivity(replyActivity);
 }
-async function generateDogImage(context) {
-
-    // Get start time
-    let startTime = new Date();
-
-    // Inform the user that image generation has started and might take few seconds
-    await context.sendActivity(`Generating an image of a dog. This may take 5-10 seconds...`);
-
-
-    const headers = { "API-Key": process.env.OPENAI_DALLE_API_KEY, "Content-Type": "application/json"};
-    const requestBody = { prompt: "a nice photo of a dog", size: "1024x1024", n: 1 };
-    const submitUrlPath = "/openai/images/generations:submit?api-version=";
-
-    console.log("\n\n*****SPECIAL_COMMANDS.JS: Starting generateDogImage...");
-
-    const response = await fetch(
-        `${OPENAI_DALLE_BASE_URL}${submitUrlPath}${OPENAI_DALLE_VERSION}`,
-        { method: "POST", headers, body: JSON.stringify(requestBody) }
-    );
-    const initJob = await response.json();
-
-    if(!initJob.id) {
-        console.error('\n\n*****SPECIAL_COMMANDS.JS: Error occurred while submitting a job', initJob);
-        return;
-    }
-
-    const jobId = initJob.id;
-    const checkJobUrlPath = "/openai/operations/images/";
-    console.log('\n*****SPECIAL_COMMANDS.JS: Dall-E job submitted, id: ', jobId);
-
-    for (let i = 0; i < 5; i++) {
-        await new Promise(resolve => setTimeout(resolve, 1500));
-
-        const res = await fetch(
-            `${OPENAI_DALLE_BASE_URL}${checkJobUrlPath}${jobId}?api-version=${OPENAI_DALLE_VERSION}`,
-            { method: "GET", headers }
-        );
-
-        const job = await res.json();
-        
-        // Added the counter (i+1) to indicate the iteration number
-        console.log('\n*****SPECIAL_COMMANDS.JS: Checking Dall-E job status...' + ' Attempt number: ' + (i+1));
-
-        if (job.status === "succeeded") {
-            const imageUrl = job.result.data[0]?.url;
-            if (imageUrl) {
-                console.log('\n*****SPECIAL_COMMANDS.JS: Dall-E image generated, url:', imageUrl);
-                await context.sendActivity(`Here's a nice photo of a dog: ${imageUrl}`);
-            }
-            break;
-        } 
-
-        if(job.status !== 'running'){
-            console.error('*****SPECIAL_COMMANDS.JS: Unknown job status:', job.status)
-        }
-    }
-
-    // Get end time
-    let endTime = new Date();
-
-    // calculate time difference in milliseconds
-    let difference = endTime - startTime;
-
-    // calculate time difference in seconds
-    let seconds = Math.floor(difference / 1000);
-
-    console.log(`\n*****SPECIAL_COMMANDS.JS:dog image Time difference is ${difference} milliseconds`);
-    console.log(`\n*****SPECIAL_COMMANDS.JS:dog image Time difference is ${seconds} seconds`);
-}
 
 const commands = {
-	'$hamburger': addToppings,
-	'$help': contactHelp,
-	'$dalle': createDalleImages,
-    '$dog': generateDogImage
+    '$hamburger': addToppings,
+    '$help': contactHelp,
+    '$dalle': createDalleImages,
 };
 
 module.exports = commands;
