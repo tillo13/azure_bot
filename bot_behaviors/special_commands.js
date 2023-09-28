6434e6684f7af14e06fd88f9bf7fbf1865d5a4c9
Andy Tillo <andy.tillo@gmail.com>
2023-09-27 20:36:41 -0700

    cleaned dog details

--- bot_behaviors/special_commands.js
+++ bot_behaviors/special_commands.js
@@ -12,7 +12,7 @@ async function createDalleImages(context) {
     await handleDalleCommand(context.activity.conversation.id, context.timestamp, prompt, numImages);
 
     const completionMessage = numImages > 1 ? `Images are on their way, might take some time.` : `Image is on its way, might take some time.`;
-    return context.sendActivity(completionMessage);
+    return await context.sendActivity(completionMessage);
 }
 
 async function addToppings(context) {
@@ -25,71 +25,82 @@ async function contactHelp(context) {
 
 async function sendMessageResponse(context, message) {
     const replyActivity = MessageFactory.text(message);
-    if (context.activity.channelId === 'slack') {
-        const thread_ts = context.activity.channelData?.SlackMessage?.event?.thread_ts 
-            || context.activity.channelData?.SlackMessage?.event?.ts;
+    
+    try {
         replyActivity.conversation = context.activity.conversation;
+        const thread_ts = context.activity.channelData?.SlackMessage?.event?.thread_ts || 
+                          context.activity.channelData?.SlackMessage?.event?.ts;
         if (!replyActivity.conversation.id.includes(thread_ts)) {
             replyActivity.conversation.id += ':' + thread_ts;
         }
+    } catch (error) {
+        console.error('Error occurred while trying to reply in the thread:', error);
     }
 
-    return context.sendActivity(replyActivity);
+    return await context.sendActivity(replyActivity);
 }
 async function generateDogImage(context) {
-    const resultsTime = "Time taken for generateDogImage";    
-    console.time(resultsTime);
-    
-    const headers = {
-        "API-Key": process.env.OPENAI_DALLE_API_KEY, 
-        "Content-Type": "application/json"
-    };
-    const requestBody = { prompt: "a nice photo of a dog", size: "1024x1024", n: 1 };
 
-    let retryCount = 0;
+    // Get start time
+    let startTime = new Date();
+
+    const headers = { "API-Key": process.env.OPENAI_DALLE_API_KEY, "Content-Type": "application/json"};
+    const requestBody = { prompt: "a nice photo of a dog", size: "1024x1024", n: 1 };
+    const submitUrlPath = "/openai/images/generations:submit?api-version=";
+
     console.log("\n\n*****SPECIAL_COMMANDS.JS: Starting generateDogImage...");
 
-    const initJob = await submitJob(headers, requestBody);
-    if (!initJob.id) return;
-    console.log('\n*****SPECIAL_COMMANDS.JS: Dall-E job submitted, id: ', initJob.id);
-
-    while(retryCount < 5) {
-        retryCount += 1;
-        console.log(`\n*****SPECIAL_COMMANDS.JS: Checking Dall-E job status... attempt: ${retryCount}`);
-
-        const job = await checkJobStatus(headers, initJob.id);
-        if (job.status === "succeeded" && job.result.data[0]?.url) {
-            console.log('*****SPECIAL_COMMANDS.JS: Dall-E image generated, url: ', job.result.data[0]?.url);
-            await context.sendActivity(`Here's a nice photo of a dog: ${job.result.data[0]?.url}`);
-            break;
-        }
-        
-        if(job.status !== 'running') {
-            console.error('*****SPECIAL_COMMANDS.JS: Unknown job status: ', job.status);
-            break;
-        }
-        await new Promise(resolve => setTimeout(resolve, 1500));
-    }
-    console.timeEnd(resultsTime);
-}
-
-async function submitJob(headers, requestBody) {
-    const response = await fetch(`${OPENAI_DALLE_BASE_URL}/images/generations:submit?api-version=${OPENAI_DALLE_VERSION}`, {
-        method: "POST",
-        headers,
-        body: JSON.stringify(requestBody)
-    });
-
-    return await response.json();
-}
-
-async function checkJobStatus(headers, id) {
     const response = await fetch(
-        `${OPENAI_DALLE_BASE_URL}/operations/images/${id}?api-version=${OPENAI_DALLE_VERSION}`, 
-        { method: "GET", headers }
+        `${OPENAI_DALLE_BASE_URL}${submitUrlPath}${OPENAI_DALLE_VERSION}`,
+        { method: "POST", headers, body: JSON.stringify(requestBody) }
     );
+    const initJob = await response.json();
 
-    return await response.json();
+    if(!initJob.id) {
+        console.error('\n\n*****SPECIAL_COMMANDS.JS: Error occurred while submitting a job', initJob);
+        return;
+    }
+
+    const jobId = initJob.id;
+    const checkJobUrlPath = "/openai/operations/images/";
+    console.log('\n*****SPECIAL_COMMANDS.JS: Dall-E job submitted, id: ', jobId);
+
+    for (let i = 0; i < 5; i++) {
+        await new Promise(resolve => setTimeout(resolve, 1500));
+
+        const res = await fetch(
+            `${OPENAI_DALLE_BASE_URL}${checkJobUrlPath}${jobId}?api-version=${OPENAI_DALLE_VERSION}`,
+            { method: "GET", headers }
+        );
+
+        const job = await res.json();
+        console.log('\n*****SPECIAL_COMMANDS.JS: Checking Dall-E job status...');
+
+        if (job.status === "succeeded") {
+            const imageUrl = job.result.data[0]?.url;
+            if (imageUrl) {
+                console.log('\n*****SPECIAL_COMMANDS.JS: Dall-E image generated, url:', imageUrl);
+                await context.sendActivity(`Here's a nice photo of a dog: ${imageUrl}`);
+            }
+            break;
+        } 
+
+        if(job.status !== 'running'){
+            console.error('*****SPECIAL_COMMANDS.JS: Unknown job status:', job.status)
+        }
+    }
+
+    // Get end time
+    let endTime = new Date();
+
+    // calculate time difference in milliseconds
+    let difference = endTime - startTime;
+
+    // calculate time difference in seconds
+    let seconds = Math.floor(difference / 1000);
+
+    console.log(`dog image Time difference is ${difference} milliseconds`);
+    console.log(`dog image Time difference is ${seconds} seconds`);
 }
 
 const commands = {
