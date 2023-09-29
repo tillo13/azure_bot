ea42cc15deb3c3f7847259fa29ca87fb55a71799
Andy Tillo <andy.tillo@gmail.com>
2023-09-29 16:24:34 -0700

    update slack_channel_id to hardcode to test7

--- bot_router.js
+++ bot_router.js
@@ -41,7 +41,7 @@ class EchoBot extends ActivityHandler {
 
 		this.onMessage(async (context, next) => {
 			try {
-                //log interaction to slack here
+                //log interaction to slack here.
                 const slack_api_token = process.env.SLACK_BOT_TOKEN;
                 const slack_channel_id = 'C05T52KUPLG';
                 console.log("\n\n**BOT_ROUTER.JS: slack_channel: ", slack_channel_id);
