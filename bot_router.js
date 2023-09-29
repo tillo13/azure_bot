6b788964c90b04c866ce9e9e66201588cc739be6
Andy Tillo <andy.tillo@gmail.com>
2023-09-29 00:20:23 -0700

    update to remove webchathandler add await

--- bot_router.js
+++ bot_router.js
@@ -32,54 +32,56 @@ class EchoBot extends ActivityHandler {
         });
 
         this.onMessage(async (context, next) => {
+            try {
+                const messageContent = context.activity.text.trim();
+                console.log('\n\n**BOT_ROUTER.JS: onMessage triggered');
+                console.log('\n\n**BOT_ROUTER.JS: Bot received a message');
+                console.log("\n\n**BOT_ROUTER.JS: Message content: ", context.activity.text);
+                        
+                if (specialCommands[messageContent]) {
+                    // If the command exists in our special commands, execute it
+                    await specialCommands[messageContent](context);
+                } else {
+                    let chatMessagesUser = await this.chatMessagesProperty.get(context, []) || [];
+                    chatMessagesUser.push({ role: "user", content: context.activity.text });
+                
+                    const current_thread_ts = context.activity.channelData && context.activity.channelData.SlackMessage && context.activity.channelData.SlackMessage.event ?
+                        context.activity.channelData.SlackMessage.event.thread_ts || context.activity.channelData.SlackMessage.event.ts : "";
+                        console.log("\n\n**BOT_ROUTER.JS: Current Slack thread timestamp: ", current_thread_ts);
+                
+                    let savedThread_ts = await this.threadproperty.get(context, "");
+                        
+                    const botCalled = context.activity.text.includes('@bot') || context.activity.text.includes('@atbot');
+                    let botInThread = await this.botInvokedFlag.get(context, false);
+                
+                    // Reset messages on new thread init
+                    if(savedThread_ts !== current_thread_ts) {
+                        chatMessagesUser = []; 
+                        await this.threadproperty.set(context, current_thread_ts);
+                    }
+                
+                    if (botCalled) {
+                        console.log("\n\n**BOT_ROUTER.JS: '@bot' or '@atbot' mentioned in the message. Bot Invoked: ", botCalled);
+                
+                        botInThread = true;
+                        await this.botInvokedFlag.set(context, botInThread);
+                        chatMessagesUser.push({ role: "user", content: context.activity.text }); 
+                    }
+                
+                    let isFirstInteraction = await this.isFirstInteraction.get(context, true);
+                    let handled = false;
+                    if (!handled) handled = await handleMessageFromMSTeams(context, chatMessagesUser, isFirstInteraction);
+                    if (!handled) handled = await handleMessageFromSlack(context, chatMessagesUser, botCalled, botInThread, savedThread_ts, current_thread_ts, PERSONALITY_OF_BOT);
+                    if (!handled) await handleDefault(context, chatMessagesUser, PERSONALITY_OF_BOT);
 
-            const messageContent = context.activity.text.trim();
-            console.log('\n\n**BOT_ROUTER.JS: onMessage triggered');
-            console.log('\n\n**BOT_ROUTER.JS: Bot received a message');
-            console.log("\n\n**BOT_ROUTER.JS: Message content: ", context.activity.text);
+                    await this.chatMessagesProperty.set(context, chatMessagesUser); 
                     
-            if (specialCommands[messageContent]) {
-                // If the command exists in our special commands, execute it
-                await specialCommands[messageContent](context);
-            } else {
-                let chatMessagesUser = await this.chatMessagesProperty.get(context, []) || [];
-                chatMessagesUser.push({ role: "user", content: context.activity.text });
-            
-            const current_thread_ts = context.activity.channelData && context.activity.channelData.SlackMessage && context.activity.channelData.SlackMessage.event ?
-                context.activity.channelData.SlackMessage.event.thread_ts || context.activity.channelData.SlackMessage.event.ts : "";
-                console.log("\n\n**BOT_ROUTER.JS: Current Slack thread timestamp: ", current_thread_ts);
-
-            
-            let savedThread_ts = await this.threadproperty.get(context, "");
-                    
-            const botCalled = context.activity.text.includes('@bot') || context.activity.text.includes('@atbot');
-            let botInThread = await this.botInvokedFlag.get(context, false);
-            
-            // Reset messages on new thread init
-            if(savedThread_ts !== current_thread_ts) {
-                chatMessagesUser = []; 
-                await this.threadproperty.set(context, current_thread_ts);
+                    await next();
+                }
+            } catch (error) {
+                console.error("**BOT_ROUTER.JS: An error occurred:", error);
             }
-            
-            if (botCalled) {
-                console.log("\n\n**BOT_ROUTER.JS: '@bot' or '@atbot' mentioned in the message. Bot Invoked: ", botCalled);
-            
-                botInThread = true;
-                await this.botInvokedFlag.set(context, botInThread);
-                chatMessagesUser.push({ role: "user", content: context.activity.text }); 
-            }
-            
-
-            let isFirstInteraction = await this.isFirstInteraction.get(context, true);
-            let handled = false;
-            if (!handled) handled = await handleMessageFromMSTeams(context, chatMessagesUser, isFirstInteraction);
-            if (!handled) handled = await handleMessageFromSlack(context, chatMessagesUser, botCalled, botInThread, savedThread_ts, current_thread_ts, PERSONALITY_OF_BOT);
-            if (!handled) handleDefault(context, chatMessagesUser, PERSONALITY_OF_BOT);
-
-            await this.chatMessagesProperty.set(context, chatMessagesUser);
-            await next();
-        }
-    });
+        });
 }
 
     async run(context) {
