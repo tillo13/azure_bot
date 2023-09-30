674e21ae3c7d15ae271a50b009c57949e22ff641
Andy Tillo <andy.tillo@gmail.com>
2023-09-30 14:48:13 -0700

    updated slack payload

--- bot_router.js
+++ bot_router.js
@@ -45,12 +45,25 @@ class EchoBot extends ActivityHandler {
 				console.log('\n\n**BOT_ROUTER.JS: onMessage triggered!');
 				console.log("\n\n**BOT_ROUTER.JS: Message content: ", context.activity.text);
 
-                //log interaction to slack here.
-                const slack_api_token = process.env.SLACK_BOT_TOKEN;
-                const slack_channel_id = 'C05T52KUPLG';
-                console.log("\n\n**BOT_ROUTER.JS: slack_channel: ", slack_channel_id);
-                console.log("\n\n**BOT_ROUTER.JS: slack token: ", slack_api_token);
-                await postMessageToSlack(slack_channel_id, null, "new message incoming", slack_api_token);
+				//log interaction to slack here.
+				const slack_api_token = process.env.SLACK_BOT_TOKEN;
+				const slack_channel_id = 'C05T52KUPLG';
+				console.log("\n\n**BOT_ROUTER.JS: slack_channel: ", slack_channel_id);
+				console.log("\n\n**BOT_ROUTER.JS: slack token: ", slack_api_token);
+				
+				// define an object to structure the Slack block message
+				const slackBlocks = {
+					"blocks": [
+						{
+							"type": "section",
+							"text": {
+								"type": "mrkdwn",
+								"text": `*Incoming interaction:*\n*Source:* ${context.activity.channelId}\n*User Requesting:* ${context.activity.from.name}\n*User Payload:* ${context.activity.text}`
+							}
+						}
+					]
+				};
+				await postMessageToSlack(slack_channel_id, null, slackBlocks, slack_api_token);
 
 
                 if (specialCommands[messageContent]) {
