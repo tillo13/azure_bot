409e6adedfece0918f026ed82e1f329b1d74e9b0
Andy Tillo <andy.tillo@gmail.com>
2023-09-30 16:50:30 -0700

    revert the teams path back to default

--- bot_behaviors/msteams.js
+++ bot_behaviors/msteams.js
@@ -1,16 +1,14 @@
 const { MessageFactory } = require('botbuilder');
 const chatCompletion = require('./chat_helper');
 
-//const PERSONALITY_OF_BOT = "You talk like a pirate. You are a helpful chatbot from Teradata. As a crucial aspect of your function, ensure you always reference past user and assistant prompts in the thread for the best understanding in order to respond effectively.";
+const PERSONALITY_OF_BOT = "You talk like a pirate. You are a helpful chatbot from Teradata. As a crucial aspect of your function, ensure you always reference past user and assistant prompts in the thread for the best understanding in order to respond effectively.";
 
 function isFromMSTeams(context) {
     return context.activity.channelId === 'msteams';
 }
 
-async function handleTeamsMessage(context, chatMessagesUser, isFirstInteraction, isFirstInteractionProperty, personality) {    
+async function handleTeamsMessage(context, chatMessagesUser, isFirstInteraction) {
     console.log('\n*****MSTEAMS.JS: Preparing to handle a message from MS Teams');
-    console.log('\n*****MSTEAMS.JS: passed Bot Personality:', personality);
-
     
     // Log the entire activity object
     console.log('\n*****MSTEAMS.JS: Teams Activity:', JSON.stringify(context.activity, null, 2));
@@ -26,10 +24,9 @@ async function handleTeamsMessage(context, chatMessagesUser, isFirstInteraction,
     if (isFirstInteraction) {
         console.log('*****MSTEAMS.JS: This is the first user interaction');
         assistantResponse = `msteams_chat_path: Hello ${username} from @bot in MS Teams!`;
-        await isFirstInteractionProperty.set(context, false); // use the PropertyAccessor
-    }  else {
+    } else {
         console.log('*****MSTEAMS.JS: This is not the first interaction. Calling OpenAI...');
-        const chatResponse = await chatCompletion(chatMessagesUser, personality, context.activity.channelId);
+        const chatResponse = await chatCompletion(chatMessagesUser, PERSONALITY_OF_BOT, context.activity.channelId);
         console.log('*****MSTEAMS.JS: Received response from OpenAI');
         assistantResponse = `MS_Teams_Chat_Path: ${chatResponse.assistantResponse}`;
     }
