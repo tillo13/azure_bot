4deb21603fd98cabb626b8504aa4608455f80b34
Andy Tillo <andy.tillo@gmail.com>
2023-09-30 21:47:56 -0700

    revert hamburger to 78554d5

--- bot_behaviors/slack.js
+++ bot_behaviors/slack.js
@@ -16,10 +16,10 @@ async function postChatHistoryToSlack(slack_channel_id, thread_ts, apiToken) {
     const messages = conversationHistory.messages.filter(msg => !msg.hasOwnProperty('bot_id'));
     
     let chatRecord = formatChatRecord(messages);
-    console.log('\n****SLACK.JS: chatRecord: ', chatRecord);
+    console.log(chatRecord);
     
     const cleanedChatRecord = cleanChatRecord(chatRecord);
-    console.log('\n****SLACK.JS: cleaned payload ready for Openai: ', cleanedChatRecord);
+    console.log('****SLACK.JS: cleaned payload ready for Openai: ', cleanedChatRecord);
   
     await postMessageToSlack(slack_channel_id, thread_ts, chatRecord, apiToken);
     return cleanedChatRecord;
@@ -56,7 +56,7 @@ async function handleSlackMessage(context, assistantResponse, letMeCheckFlag, pa
     }
     
     if (!activeThreads[thread_ts] && !context.activity.conversation.isGroup) {
-        console.log('\n****SLACK.JS: SLACK_PAYLOAD_WITHOUT_CALLING_BOT -- IGNORING! User said: ', context.activity.text);
+        console.log('****SLACK.JS: SLACK_PAYLOAD_WITHOUT_CALLING_BOT -- IGNORING! User said: ', context.activity.text);
         return;
     }
   
@@ -70,7 +70,7 @@ async function handleSlackMessage(context, assistantResponse, letMeCheckFlag, pa
         await respondToSlackThread(context, slackMessageResponse, thread_ts);
     }
   
-    console.log('\n****SLACK.JS: clean format regardless ', cleanedFormattedMessages);
+    console.log('****SLACK.JS: clean format regardless ', cleanedFormattedMessages);
     return {
         cleanedFormattedMessages: cleanedFormattedMessages,
         isActiveThread: isActiveThread
@@ -89,7 +89,7 @@ async function handleSlackMessage(context, assistantResponse, letMeCheckFlag, pa
     
             await context.sendActivity(replyActivity);
         } catch (error) {
-            console.error('\n****SLACK.JS: An error occurred while trying to reply in the thread:', error);
+            console.error('****SLACK.JS: An error occurred while trying to reply in the thread:', error);
         }
     }
 }
