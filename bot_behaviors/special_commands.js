b2920309a76058e887233e8e1e1c1a4c1b4b796e
Andy Tillo <andy.tillo@gmail.com>
2023-10-01 18:11:16 -0700

    getthreadtimestamp re-add back --small and default message2

--- bot_behaviors/special_commands.js
+++ bot_behaviors/special_commands.js
@@ -135,7 +135,7 @@ async function createDalleImages(context) {
 
 function parseArguments(messageText) {
     const defaultSettings = {
-        prompt: "a rembrandt-like painting of a dog in a field using the color palette of Teradata.",
+        prompt: "a rembrandt-like painting of a steampunk styled dog in a futuristic world using the color palette of Teradata.",
         numImages: 3,
         imageSize: "1024x1024"
     }
