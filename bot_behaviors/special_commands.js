c2414c2c48ee8b21182dc308edbde104211fce98
Andy Tillo <andy.tillo@gmail.com>
2023-10-01 19:54:11 -0700

    set and prompt to summary in msteams and webchat

--- bot_behaviors/special_commands.js
+++ bot_behaviors/special_commands.js
@@ -228,42 +228,47 @@ async function postProcess(context, thread_ts, channelId, apiToken) {
 		await addReaction(channelId, thread_ts, 'white_check_mark', apiToken);
 	}
 }
+function formatSummary(prompt, numImages, imageSize, seconds) {
+    return `Summary: We used DallE to create [${prompt}]\nNumber of images: ${numImages}\nSize of images: ${imageSize}\nTime to complete: ${duration} seconds. Thank you.`;
+}
+
 async function sendSummary(context, prompt, numImages, imageSize, seconds, thread_ts) {
-	switch (context.activity.channelId) {
-		case 'webchat':
-			// send to endpoint_formats.js
-			let message = formats.dalle_WebchatResponse(numImages, imageSize, seconds);
-			await sendMessageResponse(context, message);
-			break;
-		case 'slack':
-			let slackMessage = formats.dalle_SlackResponse(prompt, numImages, imageSize, seconds);
-			slackMessage.thread_ts = thread_ts; // Add thread_ts to the slackMessage
-			let replyActivity = {
-				type: 'message',
-				text: '',
-				channelData: slackMessage
-			};
-			try {
-				await context.sendActivity(replyActivity);
-				console.log('\n******SPECIAL_COMMANDS: Slack summary message sent successfully');
-			} catch (error) {
-				console.error('\n******SPECIAL_COMMANDS: Failed to send Slack summary message:', error);
-			}
-			break;
-		case 'msteams':
-			try {
-				let msteamsMessage = formats.dalle_msteamsResponse(numImages, imageSize, seconds);
-				await sendMessageResponse(context, msteamsMessage);
-			} catch (error) {
-				console.error('\n******SPECIAL_COMMANDS: msteams path Failed to format the message:', error);
-				message = formats.help_DefaultResponse();
-			}
-			break;
-		default:
-			// This is the default case when none of the above matches
-			let defaultMsg = formats.dalle_DefaultResponse(numImages, imageSize, seconds);
-			await sendMessageResponse(context, defaultMsg);
-	}
+    const summary = formatSummary(prompt, numImages, imageSize, seconds);
+    switch (context.activity.channelId) {
+        case 'webchat':
+            // send to endpoint_formats.js
+            let message = formats.dalle_WebchatResponse(numImages, imageSize, seconds);
+            await sendMessageResponse(context, `${summary}\n${message}`);
+            break;
+        case 'slack':
+            let slackMessage = formats.dalle_SlackResponse(prompt, numImages, imageSize, seconds);
+            slackMessage.thread_ts = thread_ts; // Add thread_ts to the slackMessage
+            let replyActivity = {
+                type: 'message',
+                text: '',
+                channelData: slackMessage
+            };
+            try {
+                await context.sendActivity(replyActivity);
+                console.log('\n******SPECIAL_COMMANDS: Slack summary message sent successfully');
+            } catch (error) {
+                console.error('\n******SPECIAL_COMMANDS: Failed to send Slack summary message:', error);
+            }
+            break;
+        case 'msteams':
+            try {
+                let msteamsMessage = formats.dalle_msteamsResponse(numImages, imageSize, seconds);
+                await sendMessageResponse(context, `${summary}\n${msteamsMessage}`);
+            } catch (error) {
+                console.error('\n******SPECIAL_COMMANDS: msteams path Failed to format the message:', error);
+                message = formats.help_DefaultResponse();
+            }
+            break;
+        default:
+            // This is the default case when none of the above matches
+            let defaultMsg = formats.dalle_DefaultResponse(numImages, imageSize, seconds);
+            await sendMessageResponse(context, `${summary}\n${defaultMsg}`);
+    }
 }
 
 function getElapsedTime(startTime, endTime) {
