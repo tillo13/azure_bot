0cf4030def6566781a179fb9bf45fd32d4a7fafa
Andy Tillo <andy.tillo@gmail.com>
2023-10-02 00:16:55 -0700

    change parse arguments logic to better pull vars

--- bot_behaviors/special_commands.js
+++ bot_behaviors/special_commands.js
@@ -30,10 +30,10 @@ const commands = new Proxy({
 });
 
 async function useShovel(context) {
-    var randNum = Math.random()*102;
-    randNum = randNum.toFixed(3); // truncates to 3 decimal places.
+	var randNum = Math.random() * 102;
+	randNum = randNum.toFixed(3); // truncates to 3 decimal places.
 
-    return sendMessageResponse(context,  `Hmm... well look at that, you just unearthed _${randNum}_ Tera-gems...`); // concatenation of the random number with the string
+	return sendMessageResponse(context, `Hmm... well look at that, you just unearthed _${randNum}_ Tera-gems...`); // concatenation of the random number with the string
 }
 
 async function contactHelp(context) {
@@ -98,73 +98,78 @@ async function sendMessageResponse(context, messageOrAttachment) {
 global.current_dalle_prompt = '';
 
 async function createDalleImages(context) {
-    const messageText = context.activity.text.replace('$dalle', '').trim();
-    const startTime = new Date().getTime();
-    const channelId = context.activity.channelId;
-    const { prompt, numImages, imageSize, originalRequestedImages } = parseArguments(messageText, channelId);
-    global.current_dalle_prompt = prompt;
-    let thread_ts;
-    
-    // Check if requested number of images is more than 10
+	const messageText = context.activity.text.replace('$dalle', '').trim();
+	const startTime = new Date().getTime();
+	const channelId = context.activity.channelId;
+	const {
+		prompt,
+		numImages,
+		imageSize,
+		originalRequestedImages
+	} = parseArguments(messageText, channelId);
+	global.current_dalle_prompt = prompt;
+	let thread_ts;
+
+	// Check if requested number of images is more than 10
 	if (originalRequestedImages > 10) {
 		await sendMessageWithThread(context, `_I think you've asked for_ ${originalRequestedImages} _images. The maximum is 10, because 10 seems like a decent max, right? However, worry not we'll still create 10 rad images. Coming right up..._`, thread_ts);
 	}
 
-    const apiToken = context.activity.channelData?.ApiToken;
-    const slackChannelId = context.activity.channelData?.SlackMessage?.event?.channel;
+	const apiToken = context.activity.channelData?.ApiToken;
+	const slackChannelId = context.activity.channelData?.SlackMessage?.event?.channel;
 
-    //Define thread_ts only if platform is slack
-    if (channelId === 'slack') {
-        thread_ts = context.activity.channelData?.SlackMessage?.event?.thread_ts ||
-                      context.activity.channelData?.SlackMessage?.event?.ts;
-        await addReaction(slackChannelId, thread_ts, 'hourglass_flowing_sand', apiToken);
-    }
+	//Define thread_ts only if platform is slack
+	if (channelId === 'slack') {
+		thread_ts = context.activity.channelData?.SlackMessage?.event?.thread_ts ||
+			context.activity.channelData?.SlackMessage?.event?.ts;
+		await addReaction(slackChannelId, thread_ts, 'hourglass_flowing_sand', apiToken);
+	}
 
-    if (!messageText) {
-		await sendMessageWithThread(context, `You did not ask for any image in particular, so get the default of... \n\n_${prompt}_\n\n\nPlease wait a moment while we wire it up...`, thread_ts);	
+	if (!messageText) {
+		await sendMessageWithThread(context, `You did not ask for any image in particular, so get the default of... \n\n_${prompt}_\n\n\nPlease wait a moment while we wire it up...`, thread_ts);
 	} else {
-        await sendMessageWithThread(context, defaultMessage(prompt, numImages, imageSize), thread_ts);
-    }
+		await sendMessageWithThread(context, defaultMessage(prompt, numImages, imageSize), thread_ts);
+	}
 
-    const filenameBase = getFileName(prompt);
+	const filenameBase = getFileName(prompt);
 
-    for (let i = 0; i < numImages; i++) {
-        const filename = `${filenameBase}_${(i+1).toString().padStart(2, '0')}.png`;
-        await sendMessageWithThread(context, `Creating ${filename}...`, thread_ts);
-        await sendTypingIndicator(context);
+	for (let i = 0; i < numImages; i++) {
+		const filename = `${filenameBase}_${(i+1).toString().padStart(2, '0')}.png`;
+		await sendMessageWithThread(context, `Creating ${filename}...`, thread_ts);
+		await sendTypingIndicator(context);
 
-        await generateImages(prompt, 1, imageSize, async (imageUrl) => {
-            const replyActivity = getReplyActivity(context, thread_ts, imageUrl);
-            await context.sendActivity(replyActivity);
-        });
-    }
+		await generateImages(prompt, 1, imageSize, async (imageUrl) => {
+			const replyActivity = getReplyActivity(context, thread_ts, imageUrl);
+			await context.sendActivity(replyActivity);
+		});
+	}
 
-    await postProcess(context, thread_ts, channelId, apiToken);
+	await postProcess(context, thread_ts, channelId, apiToken);
 
-    const endTime = new Date().getTime();
-    const seconds = getElapsedTime(startTime, endTime);
-    await sendSummary(context, prompt, numImages, imageSize, seconds, thread_ts);
+	const endTime = new Date().getTime();
+	const seconds = getElapsedTime(startTime, endTime);
+	await sendSummary(context, prompt, numImages, imageSize, seconds, thread_ts);
 }
 
 function parseArguments(messageText, channelId) {
-    const defaultSettings = {
-        prompt: "A painting reminiscent of Rembrandt, with various sprockets and springs in motion while steampunk-styled humans work alongside robots actively engaged operating Teradata's secure and trustworthy A.I. hub!",        
-        numImages: 3,
-        imageSize: channelId === 'slack' ? '512x512' : '1024x1024'
-    }
+	const defaultSettings = {
+		prompt: "A painting reminiscent of Rembrandt, with various sprockets and springs in motion while steampunk-styled humans work alongside robots actively engaged operating Teradata's secure and trustworthy A.I. hub!",
+		numImages: 3,
+		imageSize: channelId === 'slack' ? '512x512' : '1024x1024'
+	}
 
-    // Split message by space and remove empty strings
-    let splitMessage = messageText.split(" ").filter(Boolean);
+	// Split message by space and remove empty strings
+	let splitMessage = messageText.split(" ").filter(Boolean);
 
-    let numImages = defaultSettings.numImages;
-    let imageSize = defaultSettings.imageSize;
-    let promptPieces = []
-    let originalRequestedImages;
+	let numImages = defaultSettings.numImages;
+	let imageSize = defaultSettings.imageSize;
+	let promptPieces = []
+	let originalRequestedImages;
 
-    // Parse arguments
-    splitMessage.forEach((arg, index) => {
-        if (arg.startsWith("--")) {
-            let nextArg = splitMessage[index + 1];
+	// Parse arguments
+	splitMessage.forEach((arg, index) => {
+		if (arg.startsWith("--")) {
+			let nextArg = splitMessage[index + 1];
 			if (!isNaN(parseInt(arg.slice(2), 10))) {
 				// If a number follows "--", it's the number of images
 				originalRequestedImages = parseInt(arg.slice(2), 10);
@@ -174,7 +179,7 @@ function parseArguments(messageText, channelId) {
 					numImages = 10;
 				}
 			} else if (["large", "medium", "small"].includes(arg.slice(2))) {
-				switch(arg.slice(2)){
+				switch (arg.slice(2)) {
 					case 'large':
 						imageSize = '1024x1024';
 						break;
@@ -187,27 +192,27 @@ function parseArguments(messageText, channelId) {
 				}
 			}
 		} else if (!arg.startsWith("--") && (!splitMessage[index - 1] || !splitMessage[index - 1].startsWith("--"))) {
-            // If an argument does not start with "--" and is not directly following an argument that starts with "--", it's part of the prompt
-            promptPieces.push(arg);
-        }
-    });
+			// If an argument does not start with "--" and is not directly following an argument that starts with "--", it's part of the prompt
+			promptPieces.push(arg);
+		}
+	});
 
-    let settings = {
-        prompt: promptPieces.join(" ") || defaultSettings.prompt,
-        numImages: numImages,
+	let settings = {
+		prompt: promptPieces.join(" ") || defaultSettings.prompt,
+		numImages: numImages,
 		originalRequestedImages: originalRequestedImages,
 
-        // only use defaultSettings if "--size" arg is not provided in the command	
-        imageSize: imageSize || defaultSettings.imageSize,
-    }
+		// only use defaultSettings if "--size" arg is not provided in the command	
+		imageSize: imageSize || defaultSettings.imageSize,
+	}
 
-	if(channelId === 'slack' && !settings.imageSize)
-    settings.imageSize = '512x512';
-    return settings;
+	if (channelId === 'slack' && !settings.imageSize)
+		settings.imageSize = '512x512';
+	return settings;
 }
 
 function defaultMessage(prompt, numImages, imageSize) {
-     return `Summary: We are going to use DallE to create...      Prompt: ${prompt}      Number of images: ${numImages}      Size of images: ${imageSize} Please hold while we create...`;
+	return `Summary: We are going to use DallE to create...      Prompt: ${prompt}      Number of images: ${numImages}      Size of images: ${imageSize} Please hold while we create...`;
 }
 
 function getFileName(prompt) {
@@ -217,31 +222,31 @@ function getFileName(prompt) {
 
 function sendTypingIndicator(context) {
 	return context.sendActivity({
-		 type: 'typing'
+		type: 'typing'
 	});
 }
 async function sendMessageWithThread(context, message, thread_ts) {
-    const newActivity = MessageFactory.text(message);
-    newActivity.conversation = context.activity.conversation;
+	const newActivity = MessageFactory.text(message);
+	newActivity.conversation = context.activity.conversation;
 
-    if (context.activity.channelId === 'slack' && thread_ts && !newActivity.conversation.id.includes(thread_ts)) {
-        newActivity.conversation.id += ':' + thread_ts;
-    }
+	if (context.activity.channelId === 'slack' && thread_ts && !newActivity.conversation.id.includes(thread_ts)) {
+		newActivity.conversation.id += ':' + thread_ts;
+	}
 
-    await context.sendActivity(newActivity);
+	await context.sendActivity(newActivity);
 }
 
 function getReplyActivity(context, thread_ts, imageUrl) {
 	const replyActivity = MessageFactory.attachment({
-		 contentType: 'image/png',
-		 contentUrl: imageUrl,
+		contentType: 'image/png',
+		contentUrl: imageUrl,
 	});
 
 	if (context.activity.channelId === 'slack') {
-		 replyActivity.conversation = replyActivity.conversation || context.activity.conversation;
-		 if (thread_ts && !replyActivity.conversation.id.includes(thread_ts)) {
-			  replyActivity.conversation.id += ':' + thread_ts;
-		 }
+		replyActivity.conversation = replyActivity.conversation || context.activity.conversation;
+		if (thread_ts && !replyActivity.conversation.id.includes(thread_ts)) {
+			replyActivity.conversation.id += ':' + thread_ts;
+		}
 	}
 
 	return replyActivity;
@@ -251,9 +256,9 @@ async function reactToMessage(context, thread_ts, emoji) {
 	const channelId = context.activity.channelData?.SlackMessage?.event?.channel;
 	const apiToken = context.activity.channelData?.ApiToken;
 
-	return emoji === 'hourglass_flowing_sand' ? 
-		 addReaction(channelId, thread_ts, emoji, apiToken) :
-		 removeReaction(channelId, thread_ts, emoji, apiToken);
+	return emoji === 'hourglass_flowing_sand' ?
+		addReaction(channelId, thread_ts, emoji, apiToken) :
+		removeReaction(channelId, thread_ts, emoji, apiToken);
 }
 
 async function postProcess(context, thread_ts, channelId, apiToken) {
@@ -263,38 +268,38 @@ async function postProcess(context, thread_ts, channelId, apiToken) {
 	}
 }
 async function sendSummary(context, prompt, numImages, imageSize, seconds, thread_ts) {
-    if (context.activity.channelId === 'webchat') {
-        // send to endpoint_formats.js
-        let message = formats.dalle_WebchatResponse(numImages, imageSize, seconds);
-        await sendMessageResponse(context, message);
-    } else if (context.activity.channelId === 'slack') {
-        let slackMessage = formats.dalle_SlackResponse(prompt, numImages, imageSize, seconds);
-        slackMessage.thread_ts = thread_ts; // Add thread_ts to the slackMessage
-        let replyActivity = {
-            type: 'message',
-            text: '',
-            channelData: slackMessage
-        };
-        try {
-            await context.sendActivity(replyActivity);
-            console.log('\n******SPECIAL_COMMANDS: Slack summary message sent successfully');
-        } catch (error) {
-            console.error('\n******SPECIAL_COMMANDS: Failed to send Slack summary message:', error);
-        }
-    } else if (context.activity.channelId === 'msteams') {
-        try {
-            let message = formats.dalle_msteamsResponse(numImages, imageSize, seconds);
-            await sendMessageResponse(context, message);
-        } catch (error) {
-            console.error('\n******SPECIAL_COMMANDS: msteams path Failed to format the message:', error);
-            message = formats.help_DefaultResponse();
-        }
-    } else {
-        // This is the default case when none of the above matches
-        let message = formats.dalle_DefaultResponse(numImages, imageSize, seconds);
-        await sendMessageResponse(context, message);
+	if (context.activity.channelId === 'webchat') {
+		// send to endpoint_formats.js
+		let message = formats.dalle_WebchatResponse(numImages, imageSize, seconds);
+		await sendMessageResponse(context, message);
+	} else if (context.activity.channelId === 'slack') {
+		let slackMessage = formats.dalle_SlackResponse(prompt, numImages, imageSize, seconds);
+		slackMessage.thread_ts = thread_ts; // Add thread_ts to the slackMessage
+		let replyActivity = {
+			type: 'message',
+			text: '',
+			channelData: slackMessage
+		};
+		try {
+			await context.sendActivity(replyActivity);
+			console.log('\n******SPECIAL_COMMANDS: Slack summary message sent successfully');
+		} catch (error) {
+			console.error('\n******SPECIAL_COMMANDS: Failed to send Slack summary message:', error);
+		}
+	} else if (context.activity.channelId === 'msteams') {
+		try {
+			let message = formats.dalle_msteamsResponse(numImages, imageSize, seconds);
+			await sendMessageResponse(context, message);
+		} catch (error) {
+			console.error('\n******SPECIAL_COMMANDS: msteams path Failed to format the message:', error);
+			message = formats.help_DefaultResponse();
+		}
+	} else {
+		// This is the default case when none of the above matches
+		let message = formats.dalle_DefaultResponse(numImages, imageSize, seconds);
+		await sendMessageResponse(context, message);
 
-    }
+	}
 }
 
 function getElapsedTime(startTime, endTime) {
