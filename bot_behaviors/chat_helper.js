84df2ecec0e57b439527883fbf085f9f757919d3
Andy Tillo <andy.tillo@gmail.com>
2023-10-04 08:11:06 -0700

    bot response patterns add case insensitive

--- bot_behaviors/chat_helper.js
+++ bot_behaviors/chat_helper.js
@@ -155,7 +155,7 @@ try {
 
     console.log('\n\n***CHAT_HELPER.JS: Response from OpenAI API:\n', JSON.stringify(result));
     console.log('\n\n***CHAT_HELPER.JS: letMeCheckFlag is: ', letMeCheckFlag);
-    console.log('\n\n***CHAT_HELPER.JS: Is the response from chatGPT including one of the [bot_response] patterns?', bot_response_patterns.some(pattern => result.choices[0].message.content.includes(pattern)));
+    console.log('\n\n***CHAT_HELPER.JS: Is the response from chatGPT including one of the [bot_response] patterns?', bot_response_patterns.some(pattern => result.choices[0].message.content.toLowerCase().includes(pattern.toLowerCase())));
 
     // Send response back
     return {
