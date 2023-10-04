4930d1da978f633862c3ae9e55bd04dbd9e73dad
Andy Tillo <andy.tillo@gmail.com>
2023-10-04 00:14:42 -0700

    one light test refactoring chat_helper, wholesale

--- bot_behaviors/chat_helper.js
+++ bot_behaviors/chat_helper.js
@@ -1,172 +1,157 @@
-const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
+const { OpenAiClient, AzureKeyCredential } = require("@azure/openai");
+
+const ENDPOINT = process.env.OPENAI_API_BASE_URL;
+const API_KEY = process.env.OPENAI_API_KEY;
+const DEPLOYMENT_ID = process.env.OPENAI_API_DEPLOYMENT;
 
 const MAX_OPENAI_TOKENS = 400;
-
-function validateOpenAITokens(tokens) {
-    if (tokens <= 0 || tokens > 4096) {
-        console.error('Invalid setting for MAX_OPENAI_TOKENS. It should be between 1 and 4096.');
-        return;
-    }
-    return tokens;
-}
-
 const bot_response_patterns = [
-    "as an artificial intelligence",
-    "as a digital assistant",
-    "as a computer program",
-    "as a helpful assistant",
-    "as a virtual assistant",
-    "as a language model",
-    "access to personal information",
-    "access to previous conversations",
-    "shared in previous conversations",
-    "have access to past conversations",
-    "just a virtual assistant",
-    "as a text-based AI",
-    "as an AI system",
-    "being a digital entity",
-    "as an AI",
-    "as a machine learning model",
-    "as a AI assistant",
-    "as a machine learning assistant",
-    "access to the conversation",
-    "have access to personal data",
-    "not privy to that information", 
-    "just a helpful assistant"   
-    // Include any more patterns...
+  "as an artificial intelligence",
+  "as a digital assistant",
+  "as a computer program",
+  "as a helpful assistant",
+  "as a virtual assistant",
+  "as a language model",
+  "access to personal information",
+  "access to previous conversations",
+  "shared in previous conversations",
+  "have access to past conversations",
+  "just a virtual assistant",
+  "as a text-based AI",
+  "as an AI system",
+  "being a digital entity",
+  "as an AI",
+  "as a machine learning model",
+  "as a AI assistant",
+  "as a machine learning assistant",
+  "access to the conversation",
+  "have access to personal data",
+  "not privy to that information", 
+  "just a helpful assistant"   
+  // Include any more patterns...
 ];
 
-function shouldRequery(responseContent) {
-    const lowerCasedResponse = responseContent.toLowerCase();
-    
-    return bot_response_patterns.some(pattern =>
-       lowerCasedResponse.includes(pattern.toLowerCase())
-    );
-}
-
-function formatChatPayload(chatMessages, cleanedFormattedMessages, lastUserMessage) {
-
-    const checkMessage = "Let me check our past conversations, one moment...";
-    const lastIndex = chatMessages.map(item => item.content).lastIndexOf(checkMessage);
-
-    if (lastIndex > -1) {
-        chatMessages.push(
-            { role: 'assistant', content: "I could not find a suitable response to your latest message. Please respond with your conversation history to this point and I will investigate." },
-            { role: 'user', content: `Certainly, here is what I have said so far in this thread, with timestamps: ${cleanedFormattedMessages}.  Read these messages to see if you can answer my latest question of: ${lastUserMessage}.  If you cannot find a suitable response in what I have provided, state that you are sorry but couldn not find a match and suggest a topic related to what we have discussed.` }
-        );
-    }  
-    return chatMessages;
-}
-
-async function chatCompletion(chatTexts, roleMessage, channelId, isActiveThread) {
-
-  console.log('\n\n***CHAT_HELPER.JS: Is the slack thread active?:', isActiveThread);
-  console.log('\n\n***CHAT_HELPER.JS: The incoming payload is coming from: ', channelId);
-
-  console.log('\n\n***CHAT_HELPER.JS: OpenAI API Base URL: ', process.env.OPENAI_API_BASE_URL);
-  console.log('\n\n***CHAT_HELPER.JS: OpenAI API Deployment: ', process.env.OPENAI_API_DEPLOYMENT);
-
-  ///test dalle items showing in .env
-  console.log('\n\n***CHAT_HELPER.JS: OpenAI DallE API Base URL: ', process.env.OPENAI_DALLE_BASE_URL);
-  console.log('\n\n***CHAT_HELPER.JS: OpenAI DallE API Deployment: ', process.env.OPENAI_DALLE_VERSION);
-
-
-
-    const endpoint = process.env.OPENAI_API_BASE_URL;
-    const client = new OpenAIClient(endpoint, new AzureKeyCredential(process.env.OPENAI_API_KEY));
-    const deploymentId = process.env.OPENAI_API_DEPLOYMENT;
-    const validatedTokens = validateOpenAITokens(MAX_OPENAI_TOKENS);
-    if(!validatedTokens) return;
-
-    let chatMessages = Array.isArray(chatTexts) ? chatTexts : [];
-
-    if (!chatMessages.length || chatMessages[0].role !== "system") {
-        chatMessages.unshift({ role: "system", content: roleMessage });
-    }
-// Fetch the last user message before calling `formatChatPayload`
-const lastUserMessageObj = chatMessages.filter((msg) => msg.role === 'user').pop();
-const lastUserMessage = lastUserMessageObj ? lastUserMessageObj.content : '';
-
-// Print out the user messages so far via chat messages
-const userMessages = chatMessages.filter((msg) => msg.role === 'user');
-console.log('\n\n***CHAT_HELPER.JS -> USER MESSAGES SO FAR via chatmessages:\n');
-userMessages.forEach((msg, index) => {
-    console.log(`\n${index + 1}. ${msg.content}\n`);
-});
-
-const oldChatMessages = JSON.stringify(chatMessages);
-
-// Track original length of conversation
-const originalLength = chatMessages.length;
-
-// Separate out each kind of message
-let newCleanChatMessages = chatMessages.filter(item => !item.content.startsWith('Certainly, here is what I have said so far'));
-console.log('\n\n***CHAT_HELPER.JS: Sending Payload to OpenAI via first call: ', newCleanChatMessages);
-
-const certainlyMessages = chatMessages.filter(item => item.content.startsWith('Certainly, here is what I have said so far'));
-
-if (certainlyMessages.length > 0) {
-  // If there are any 'Certainly' messages, only keep the last one
-  newCleanChatMessages.push(certainlyMessages[certainlyMessages.length - 1]);
-}
-
-// More efficient deduplication by converting to JSON (prevents issues with object references)
-let seenMessages = new Set(newCleanChatMessages.map(JSON.stringify));
-
-// Remove duplicate messages
-newCleanChatMessages = Array.from(seenMessages).map(JSON.parse);
-
-// How many duplicates were removed
-const duplicatesRemoved = originalLength - newCleanChatMessages.length;
-
-if (duplicatesRemoved > 0) {
-  console.log(`\n\n***CHAT_HELPER.JS: CLEANED CODE OF THIS MANY DUPLICATES: ${duplicatesRemoved}`);
-  console.log('\n\n***CHAT_HELPER.JS: AFTER DUPLICATES REMOVED, CLEANED PAYLOAD: \n', newCleanChatMessages);
-} else {
-  console.log('\n\n***CHAT_HELPER.JS: CLEAN PAYLOAD, NO DUPLICATION.');
-}
-
-// Start interacting with OpenAI
-try {
-  let result = await client.getChatCompletions(deploymentId, newCleanChatMessages, { maxTokens: validatedTokens });
-
-  if (result && result.choices[0]?.message?.content) {
-    // Check if assistant wants to requery message
-    let letMeCheckFlag = shouldRequery(result.choices[0].message.content);
-
-    if (letMeCheckFlag) {
-      // If so, update payload and requery
-      let looped_through_payload = newCleanChatMessages.filter(msg => msg.role === 'user').map(item => item.content).join(', ');
-      newCleanChatMessages = formatChatPayload(newCleanChatMessages, looped_through_payload, lastUserMessage);
-      console.log('\n\n***CHAT_HELPER.JS: Sending Payload to OpenAI via 2nd branch: ', newCleanChatMessages);
-
-
-      if(JSON.stringify(newCleanChatMessages) !== oldChatMessages)
-        console.log('\n\n!!!IMPORTANT!!!! CHAT_HELPER.JS: *** Payload was updated after removing duplicates. This was triggered by the letMeCheckFlag from the handleSlackMessage() function in slack.js. The new payload: \n', newCleanChatMessages);
-
-      result = await client.getChatCompletions(deploymentId, newCleanChatMessages, { maxTokens: validatedTokens });
-    }
-
-    console.log('\n\n***CHAT_HELPER.JS: Response from OpenAI API:\n', JSON.stringify(result));
-    console.log('\n\n***CHAT_HELPER.JS: letMeCheckFlag is: ', letMeCheckFlag);
-
-    // Send response back
-    return {
-      'assistantResponse': result.choices[0].message.content,
-      'requery': letMeCheckFlag,
-      'letMeCheckFlag': letMeCheckFlag
-    };
-  } else {
-    console.log('\n\n***CHAT_HELPER.JS: No content in API response');
-    return {
-      'assistantResponse': "I'm sorry, I couldn't understand that. Could you please try again?",
-      'requery': false,
-      'letMeCheckFlag': false
-    };
+/**
+ * Validates token length
+ * @param {number} tokens - Token length
+ * @returns {number} - Returns token only if it's valid
+ */
+function validateOpenAiTokens(tokens) {
+  if (tokens <= 0 || tokens > 4096) {
+    console.error('Invalid setting for MAX_OPENAI_TOKENS. Should be between 1 and 4096.');
+    return;
   }
-} catch (error) {
-  console.error("\n\n***CHAT_HELPER.JS:An error occurred while interacting with OpenAI API", error);
-  throw error;
+  return tokens;
 }
-        }
+
+/**
+ * Checks requery need in response
+ * @param {string} responseContent 
+ * @returns {boolean}
+ */
+function shouldRequery(responseContent) {
+  const lowerCasedResponse = responseContent.toLowerCase();
+  return bot_response_patterns.some(pattern => 
+    lowerCasedResponse.includes(pattern.toLowerCase())
+  );
+}
+
+/**
+ * Formats chat payload
+ * @param {object[]} chatMessages 
+ * @param {string} cleanedFormattedMessages 
+ * @param {string} lastUserMessage 
+ * @returns {object[]} - Returns formatted chat payload
+ */
+function formatChatPayload(chatMessages, cleanedFormattedMessages, lastUserMessage) {
+  const checkMessage = "Let me check our past conversations, one moment...";
+  const lastIndex = chatMessages.map(item => item.content).lastIndexOf(checkMessage);
+  if (lastIndex > -1) {
+    const assistantMsg = {
+      role: 'assistant',
+      content: "I could not find a suitable response to your latest message. Please respond with your conversation history..."
+    };
+    const userMsg = { 
+      role: 'user', 
+      content: `Certainly, here is what I have said so far in this thread, with timestamps: ${cleanedFormattedMessages}...`
+    };
+    chatMessages.push(assistantMsg, userMsg);
+  }  
+  return chatMessages;
+}
+
+/**
+ * Initiates chat completion
+ * @param {object[]} chatTexts 
+ * @param {string} roleMessage 
+ * @param {number} channelId 
+ * @param {boolean} isActiveThread 
+ * @returns {object} - Returns assistant response, requery flag and letMeCheckFlag 
+ */
+async function chatCompletion(chatTexts, roleMessage, channelId, isActiveThread) {
+  const validatedTokens = validateOpenAiTokens(MAX_OPENAI_TOKENS);
+  if(!validatedTokens) return;
+
+  let chatMessages = Array.isArray(chatTexts) ? chatTexts : [];
+  if (!chatMessages.length || chatMessages[0].role !== "system") {
+    chatMessages.unshift({ role: "system", content: roleMessage });
+  }
+  
+  const lastUserMessageObj = chatMessages.filter((msg) => msg.role === 'user').pop();
+  const lastUserMessage = lastUserMessageObj ? lastUserMessageObj.content : '';
+  const oldChatMessages = JSON.stringify(chatMessages);
+
+  let newCleanChatMessages = deduplicateMessages(chatMessages);
+  try {
+    let result = await getCompletions(newCleanChatMessages);
+    let letMeCheckFlag = false;
+    if (result && result.choices[0]?.message?.content) {
+      letMeCheckFlag = shouldRequery(result.choices[0].message.content);
+      if (letMeCheckFlag) {
+        let looped_through_payload = newCleanChatMessages
+          .filter(msg => msg.role === 'user')
+          .map(item => item.content).join(', ');
+        
+        newCleanChatMessages = formatChatPayload(newCleanChatMessages, looped_through_payload, lastUserMessage);
+
+        if(JSON.stringify(newCleanChatMessages) !== oldChatMessages)
+          console.log('\n\n!!!IMPORTANT!!!! The new payload: \n', newCleanChatMessages);
+          
+        result = await getCompletions(newCleanChatMessages);
+      }
+      return { 'assistantResponse': result.choices[0].message.content, 'requery': letMeCheckFlag, 'letMeCheckFlag': letMeCheckFlag };
+    } else {
+      console.log('No content in API response');
+      return { 'assistantResponse': "I'm sorry, I couldn't understand.", 'requery': false, 'letMeCheckFlag': false };
+    }
+  } catch (error) {
+    console.error(" An error occurred while interacting with OpenAI API", error);
+    throw error;
+  }
+}
+
+/**
+ * Deduplicates messages
+ * @param {object[]} chatMessages 
+ * @returns {object[]} - Returns deduplicated messages
+ */
+function deduplicateMessages(chatMessages) {
+  const newCleanChatMessages = chatMessages.filter(item => !item.content.startsWith('Certainly'));
+  const certainlyMessages = chatMessages.filter(item => item.content.startsWith('Certainly'));
+  if (certainlyMessages.length > 0) newCleanChatMessages.push(certainlyMessages[certainlyMessages.length - 1]);
+  const seenMessages = new Set(newCleanChatMessages.map(JSON.stringify));
+  newCleanChatMessages = Array.from(seenMessages).map(JSON.parse);
+  return newCleanChatMessages;
+}
+
+/**
+ * Fetches completions
+ * @param {object[]} chatMessages 
+ * @returns {object} - Returns fetch completion
+ */
+async function getCompletions(chatMessages) {
+  const client = new OpenAiClient(ENDPOINT, new AzureKeyCredential(API_KEY));
+  return await client.getChatCompletions(DEPLOYMENT_ID, chatMessages, { maxTokens: MAX_OPENAI_TOKENS });
+}
+
 module.exports = chatCompletion;
