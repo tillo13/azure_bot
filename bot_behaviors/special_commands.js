24842cafcf38e25b8f5be9fdc5516377a87d8d01
Andy Tillo <andy.tillo@gmail.com>
2023-10-05 08:26:37 -0700

    fetch id using api call in jira --move more function to jira_utils

--- bot_behaviors/special_commands.js
+++ bot_behaviors/special_commands.js
@@ -35,40 +35,16 @@ const commands = new Proxy({
 });
 
 async function createJiraTask(context) {
-	// Extract the task description from the context
-	const description = context.activity.text.replace('$createjira ', '');
-	// Set the summary to be a constant string
-	const summary = 'Test from teams';
+    const description = context.activity.text.replace('$createjira ', '');
+    const summary = 'Test from teams';
 
-	try {
-		// Call the function to create a Jira task
-		const task = await jira_utils.createJiraTask(summary, description);
-		// If the request is successful, send a message back to the user
-		return sendMessageResponse(context, `Task ${task.key} has been created under ADD-615 with the description: ${description}`);
-	} catch (err) {
-		console.error(err.message);
-		return sendMessageResponse(context, `Error creating JIRA task: ${err.message}`);
-	}
+    const responseMessage = await jira_utils.createJiraTask(summary, description);
+    return sendMessageResponse(context, responseMessage);
 }
 
-
 async function getJiraIssues(context) {
-    let jira_server = process.env['2023sept8_JIRA_SERVER'];
-    let JiraIssues;
-    try {
-        // Call the function to get Jira issues
-        JiraIssues = await jira_utils.getIssuesAssignedToCurrentUser();
-    } catch(error) {
-        console.log(error);
-        return sendMessageResponse(context, `Error retrieving JIRA issues: ${error.message}`);
-    }
-
-    // Format the retrieved issues for sending
-    let formatted_issues = JiraIssues.issues.map(issue =>
-        `*${issue.key}* ${issue.fields.summary}\n<${jira_server}/browse/${issue.key}|Go to Issue>`
-    ).join("\n-----------------\n");
-
-    return sendMessageResponse(context, formatted_issues);
+    const responseMessage = await jira_utils.getIssuesAssignedToCurrentUser();
+    return sendMessageResponse(context, responseMessage);
 }
 
 async function resetChatPayload(context, chatMessagesProperty) {
