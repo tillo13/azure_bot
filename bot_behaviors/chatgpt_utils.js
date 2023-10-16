c6128b9d7e315372a1dd896a16b561e388f16eea
Andy Tillo <andy.tillo@gmail.com>
2023-10-16 15:33:12 -0700

    set gpt35 payload for chat_util function

--- bot_behaviors/chatgpt_utils.js
+++ bot_behaviors/chatgpt_utils.js
@@ -9,8 +9,41 @@ async function getAADObjectIdFromDB(chatID) {
 }
 
 async function getLast24HrInteractionPerUserFromDB(aadObjectID) {
-    const result = await fetchLast24HrInteractionPerUserFromDB(aadObjectID);
-    return result;
+    try {
+        // fetch the last 24 hour interaction from the database
+        const result = await fetchLast24HrInteractionPerUserFromDB(aadObjectID);
+        
+        // sort the interaction based on 'hourssincelastinteraction' in ascending order
+        const sortedResult = result.sort((a, b) => a.hourssincelastinteraction - b.hourssincelastinteraction);
+        
+        // Define the initial system message
+        const chatPayload = [
+            {
+                role: "system",
+                content: "You are polite, sophisticated, chatbot assistant."
+            }
+        ]
+
+        // mapping the sorted results to the required format and pushing into the chat payload
+        sortedResult.forEach(message => {
+            chatPayload.push(
+                {
+                    role: "user",
+                    content: message.user_invoke_message,
+                },
+                {
+                    role: "assistant",
+                    content: message.bot_response_message,
+                }
+            );
+        });
+
+        console.log("Success! Chat payload is:", JSON.stringify(chatPayload));
+        return chatPayload;
+
+    } catch (error) {
+        console.error("An error occurred while retrieving and formating interactions: ", error);
+    }
 }
 
 module.exports = {
