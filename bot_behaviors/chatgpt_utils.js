55ec6b208a07acacf2c1a47115e7b19f670062c0
Andy Tillo <andy.tillo@gmail.com>
2023-10-16 16:25:14 -0700

    set user_only set of datapoints

--- bot_behaviors/chatgpt_utils.js
+++ bot_behaviors/chatgpt_utils.js
@@ -10,7 +10,7 @@ async function getAADObjectIdFromDB(chatID) {
 
 async function getLast24HrInteractionPerUserFromDB(aadObjectID) {
     try {
-        // fetch the last 24 hour interaction from the database
+        // fetch the last 24-hour interaction from the database
         const result = await fetchLast24HrInteractionPerUserFromDB(aadObjectID);
         
         // sort the interaction based on 'hourssincelastinteraction' in descending order
@@ -20,9 +20,12 @@ async function getLast24HrInteractionPerUserFromDB(aadObjectID) {
         const chatPayload = [
             {
                 role: "system",
-                content: "You are polite, sophisticated, chatbot assistant."
+                content: "You are a polite, sophisticated, chatbot assistant."
             }
         ];
+
+        // This array stores each message chronologically
+        const userMessages = [];
 
         // mapping the sorted results to the required format and pushing into the chat payload
         sortedResult.forEach(message => {
@@ -36,19 +39,23 @@ async function getLast24HrInteractionPerUserFromDB(aadObjectID) {
                     content: message.bot_response_message,
                 }
             );
+            userMessages.unshift("~"+ parseFloat(message.hourssincelastinteraction).toFixed(3) + " hours ago, I said [" + message.user_invoke_message + "].");
         });
 
-        console.log("\n*******CHATGPT_UTILS.JS: The most recent user message in the last 60 mins:", sortedResult[sortedResult.length - 1].user_invoke_message)
+        // Join all user messages from the last 24 hours chronologically into a single string
+        const userMessagesinLast24hrswithapproximateTimessortedChronologically = userMessages.join(", ");
+
+        console.log("\n*******CHATGPT_UTILS.JS: The user messages in the last 24 hours with approximate times sorted chronologically: ", userMessagesinLast24hrswithapproximateTimessortedChronologically);
+        
+        console.log("\n*******CHATGPT_UTILS.JS: The most recent user message in the last 24 hours: ", sortedResult[sortedResult.length - 1].user_invoke_message);
         console.log("\n*******CHATGPT_UTILS.JS: The most recent user message was received: ", sortedResult[sortedResult.length - 1].hourssincelastinteraction, "hours ago.");
-        console.log("\n*******CHATGPT_UTILS.JS: The oldest user message in the last 60 mins is:", sortedResult[0].user_invoke_message)
+        console.log("\n*******CHATGPT_UTILS.JS: The oldest user message in the last 24 hours: ", sortedResult[0].user_invoke_message);
         console.log("\n*******CHATGPT_UTILS.JS: The oldest user message was received", sortedResult[0].hourssincelastinteraction, "hours ago.");
 
         return chatPayload;
-        
-
 
     } catch (error) {
-        console.error("An error occurred while retrieving and formating interactions ", error);
+        console.error("An error occurred while retrieving and formatting interactions ", error);
     }
 }
 
