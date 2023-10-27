54eb2d04d6bf9f1115d715fa7050eb2cf54659a2
Tillo <andytillo@gmail.com>
2023-10-27 12:56:28 -0700

    splitting chat_helper to sub files2

--- bot_behaviors/chat_helper_utils/chat_configs.js
+++ bot_behaviors/chat_helper_utils/chat_configs.js
@@ -2,12 +2,17 @@ function validateOpenAITokens(tokens) {
     let isValid = tokens > 0 && tokens <= 4096;
     
     // log the token count and if they are valid
-    console.log(`\n\nCHAT_CONFIG.JS: Token count: ${tokens}, is valid: ${isValid}`);
+    console.log(`Token count: ${tokens}, is valid: ${isValid}`);
 
     if (!isValid) {
-        console.error('\n\nCHAT_CONFIG.JS: Invalid setting for MAX_OPENAI_TOKENS. It should be between 1 and 4096.');
-        return;
+      console.error('Invalid setting for MAX_OPENAI_TOKENS. It should be between 1 and 4096.');
+      return;
     }
 
     return tokens;
 }
+
+module.exports = {
+  validateOpenAITokens,
+  // other functions can go here in the future...
+};
