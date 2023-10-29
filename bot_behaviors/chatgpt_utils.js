2d64c7f506324853f41ce7e6803be9be631c427b
Tillo <andytillo@gmail.com>
2023-10-29 10:50:34 -0700

    POST GOLD refactor chatgpt_utils.js2

--- bot_behaviors/chatgpt_utils.js
+++ bot_behaviors/chatgpt_utils.js
@@ -1,56 +1,42 @@
 const global_configs = require('../utilities/global_configs.js');
-const { fetchAADObjectIdFromDB, fetchLast24HrInteractionPerUserFromDB } = require('../utilities/postgres_utils');
+const {
+    fetchAADObjectIdFromDB,
+    fetchLast24HrInteractionPerUserFromDB
+} = require('../utilities/postgres_utils');
 
-const processChatIDInteraction = async (chatID) => {
-    let fetchedData;
-    const isArray = Array.isArray(chatID);
-    const dataFunc = isArray ? console.log('arrayWorks') && fetchAADObjectIdFromDB : console.log('singleWorks') && fetchAADObjectIdFromDB;
+async function getAADObjectIdFromDB(chatID) {
+    return Array.isArray(chatID) ? await handleMultipleChatIDs(chatID) : await handleSingleChatID(chatID);
+}
 
-    if(isArray) {
-        for(const id of chatID) {
-            console.log(`*******CHATGPT_UTILS.JS: Processing a chatID: ${id}`);
-            fetchedData =  await handleSingleChatID(id, dataFunc);
-            if(fetchedData?.length > 0) return fetchedData;
-        };
-    } else {
-        fetchedData = await handleSingleChatID(chatID, dataFunc);
+async function handleMultipleChatIDs(chatIDs) {
+    console.log("\n*******CHATGPT_UTILS.JS: Found an array of chatIDs");
+    for (const id of chatIDs) {
+        const result = await handleSingleChatID(id);
+        if (result.length > 0) return result;
+    }
+    console.log("\n*******CHATGPT_UTILS.JS: No match found in the database for any chatIDs in the given array");
+    return [];
+}
+
+async function handleSingleChatID(chatID) {
+    console.log("\n*******CHATGPT_UTILS.JS: Checking for a single chatID");
+    const result = await fetchAADObjectIdFromDB(chatID);
+    result.length > 0 ? console.log(`\n*******CHATGPT_UTILS.JS: Found a match in the database for chatID: ${chatID}`) : console.log("\n*******CHATGPT_UTILS.JS: No match found in the database for the given chatID");
+    return result;
+}
+
+
+async function getLast24HrInteractionPerUserFromDB(aadObjectID) {
+    console.log("\n*******CHATGPT_UTILS.JS: getLast24HrInteractionPerUserFromDB func is running");
+
+    const result = await fetchLast24HrInteractionPerUserFromDB(aadObjectID);
+    if (result.length === 0) {
+        console.error("\n*******CHATGPT_UTILS.JS: No interactions found in last 24 hours for the user.");
+        return [];
     }
 
-    console.log(`*******CHATGPT_UTILS.JS: No match found in the database for ${isArray ? 'any chatIDs in the given array' : 'the given chatID'}`);
-    return fetchedData;
+    return global_configs.dbRecreationOfGptPayload ? formatResults(result) : getLastUserInteraction(result);
 }
-
-
-const handleSingleChatID = async (chatID, fetchFunc) => {
-    const data = await fetchFunc(chatID);
-
-    if(data?.length > 0) {
-        console.log(`*******CHATGPT_UTILS.JS: Found a match in the database for chatID: ${chatID}`);
-    }
-    return data;
-}
-
-const getLast24HrInteractionPerUserFromDB = async (aadObjectID) => {
-    const result =  await fetchLast24HrInteractionPerUserFromDB(aadObjectID);
-    return handleResults(aadObjectID, result, global_configs.dbRecreationOfGptPayload);
-}
-
-const handleResults = (aadObjectID, result, useGlobalConfig) => {
-    let chatPayload = [];
-
-    if(!useGlobalConfig) {
-        console.log("*******CHATGPT_UTILS.JS: getLast24HrInteractionPerUserFromDB func Skipping DB Recreation of GPT Payload as the global config is set to false.");
-        chatPayload = getLastUserInteraction(result);
-    } else { 
-        console.log("*******CHATGPT_UTILS.JS: getLast24HrInteractionPerUserFromDB func Continuing with DB Recreation of GPT Payload as the global config is set to true.");
-        if (result.length > 0) {
-            chatPayload = formatResults(result);
-        }
-    }
-    console.error("*******CHATGPT_UTILS.JS: No interactions found in last 24 hours for the user.");
-    return chatPayload;
-}
-
 
 const formatResults = (result) => {
     const sortedResult = result.sort((a, b) => b.hourssincelastinteraction - a.hourssincelastinteraction);
@@ -70,37 +56,30 @@ const formatResults = (result) => {
     return chatPayload;
 }
 
-
 const getLastUserInteraction = (result) => {
-    if (result.length > 0){
-        const lastInteraction = result[result.length - 1];
-        const chatPayload = [
-            {
-                role: "system",
-                content: "You are a polite, sophisticated, chatbot assistant that always checks historic conversations before using other resources to find answers."
-            },
-            {
-                role: "user",
-                content: lastInteraction.user_invoke_message,
-            },
-            {
-                role: "assistant",
-                content: lastInteraction.bot_response_message,
-            }
-        ];
-        return chatPayload;
-    } 
-    console.error("*******CHATGPT_UTILS.JS: getLastUserInteraction No interactions found for the user.");
-    return [];
+    const lastInteraction = result[result.length - 1];
+    return [
+        { role: "system", content: "You are a polite, sophisticated, chatbot assistant that always checks historic conversations before using other resources to find answers." },
+        { role: "user", content: lastInteraction.user_invoke_message },
+        { role: "assistant", content: lastInteraction.bot_response_message },
+
+    ];
 }
 
-const recreateGptPayloadViaDB = async (aadObjectID) => {
+async function recreateGptPayloadViaDB(aadObjectID) {
+    console.log("\n*******CHATGPT_UTILS.JS: recreateGptPayloadViaDB func is running");
+
     const result = await fetchLast24HrInteractionPerUserFromDB(aadObjectID);
-    return handleResults(aadObjectID, result, true);
+    if (result.length === 0) {
+        console.error("\n*******CHATGPT_UTILS.JS: No interactions found for the user in last 24 hours.");
+        return [];
+    }
+
+    return formatResults(result);
 }
 
 module.exports = {
     getAADObjectIdFromDB,
     getLast24HrInteractionPerUserFromDB,
     recreateGptPayloadViaDB
-};  
+};
