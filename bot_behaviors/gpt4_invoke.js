d1a2b9bcf4c4ee0ba79acbed3b637667576bf7e9
Tillo <andytillo@gmail.com>
2023-10-30 11:46:21 -0700

    more logging4

--- bot_behaviors/gpt4_invoke.js
+++ bot_behaviors/gpt4_invoke.js
@@ -1,33 +1,41 @@
 const axios = require('axios');
 
+// Get environment variables
 const OPENAI_API_KEY = process.env['2023oct24_OPENAI_GPT4_API_KEY'];
 const OPENAI_API_BASE_URL = process.env['2023oct24_OPENAI_GPT4_API_BASE_URL'];
 const OPENAI_GPT4_ENGINE = process.env['2023oct24_OPENAI_GPT4_32K_API_ENGINE_DEPLOYMENT'];
 
+// Set axios defaults
 axios.defaults.baseURL = OPENAI_API_BASE_URL;
 axios.defaults.headers['Content-Type'] = 'application/json';
 axios.defaults.headers['Authorization'] = `Bearer ${OPENAI_API_KEY}`;
 
-console.log('\n\n[DEBUG for gpt4_invoke.js]: Using the following OpenAI settings:', OPENAI_API_KEY, OPENAI_API_BASE_URL, OPENAI_GPT4_ENGINE);
-
+// Function to call OpenAI API
 const invokeOpenaiGpt4 = async (prompt) => {
+    // Prepare payload
     const payload = {
         model: OPENAI_GPT4_ENGINE,
         messages: [
-            {"role": "system", "content": "You are an AI assistant that helps people find information."},
-            {"role": "user", "content": prompt}
+            { role: "system", content: "You are an AI assistant."},
+            { role: "user", content: prompt }
         ],
         max_tokens: 800,
         temperature: 0.7
     };
 
+    // Print payload
     console.log('\n\n[DEBUG for gpt4_invoke.js]: Sending payload to OpenAI:', payload);
 
+    // Define endpoint
+    const endpoint = `/api/2023-07-01-preview/engines/${OPENAI_GPT4_ENGINE}/chats`;
+
+    // Print endpoint
+    console.log(`\n\n[DEBUG for gpt4_invoke.js]: Endpoint: ${axios.defaults.baseURL}${endpoint}`);
+
+    // Make request and handle response
     try {
-        const endpoint = `/api/2023-07-01-preview/engines/${OPENAI_GPT4_ENGINE}/chats`;
         const response = await axios.post(endpoint, payload);
         console.log('\n\n[DEBUG for gpt4_invoke.js]: OpenAI API Chat Response:', response.data);
-        // you may need to change the following line depending on the structure of the response data
         return response.data.choices[0].message.content;
     } catch (error) {
         console.error('\n\n[DEBUG for gpt4_invoke.js]: Error', error.message);
