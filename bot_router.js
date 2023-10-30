e7fcba3a37b5ed0d664fa1e4d21fd67bdd13a1b3
Tillo <andytillo@gmail.com>
2023-10-29 19:58:02 -0700

    try clean bot_router

--- bot_router.js
+++ bot_router.js
@@ -1,169 +1,109 @@
-//2023oct12 update to save to db
 const { botRouterSaveDataToPostgres } = require('./utilities/postgres_utils');
+const { handleMessageFromMSTeams, handleMessageFromSlack, handleDefault } = require('./bot_behaviors/message_handler');
+const { ActivityHandler, MessageFactory } = require('botbuilder');
+const specialCommands = require('./bot_behaviors/special_commands');
+const { postMessageToSlack } = require('./bot_behaviors/slack_utils');
+
+const BOT_PERSONALITY = "You are thorough, polite, helpful and courteous.";
 
 const PATH_CONFIGS = {
-	'msteams': {
-		personality: "You are thorough, polite, helpful and courteous.",
-		welcomeMessage: "Howdy! Welcome to our Microsoft Teams chat!",
-		messagePrefix: "teams_path"
-	},
-	'slack': {
-		personality: "You are thorough, polite, helpful and courteous.",
-		welcomeMessage: "Greetings earthling, welcome to our Slack channel!",
-		messagePrefix: "slack_path"
-	},
-	'webchat': {
-		personality: "You are thorough, polite, helpful and courteous.",
-		welcomeMessage: "Ahoy! Welcome to our webchat channel!",
-		messagePrefix: "default_path"
-	}
+  'msteams': {
+    personality: BOT_PERSONALITY,
+    welcomeMessage: "Howdy! Welcome to our Microsoft Teams chat!",
+    messagePrefix: "teams_path"
+  },
+  'slack': {
+    personality: BOT_PERSONALITY,
+    welcomeMessage: "Greetings earthling, welcome to our Slack channel!",
+    messagePrefix: "slack_path"
+  },
+  'webchat': {
+    personality: BOT_PERSONALITY,
+    welcomeMessage: "Ahoy! Welcome to our webchat channel!",
+    messagePrefix: "default_path"
+  }
 };
 
-const {
-	ActivityHandler,
-	MessageFactory
-} = require('botbuilder');
-const {
-	handleMessageFromMSTeams,
-	handleMessageFromSlack,
-	handleDefault
-} = require('./bot_behaviors/message_handler');
-const {
-	postMessageToSlack
-} = require('./bot_behaviors/slack_utils');
-const specialCommands = require('./bot_behaviors/special_commands');
-
-const WELCOMED_USER = 'welcomedUserProperty';
-const CHAT_MESSAGES = 'chatMessagesProperty';
+const MEMBERS_ADDED = 'membersAddedProperty';
+const USER_MESSAGES = 'userMessagesProperty';
 const THREAD_TS = 'thread_ts';
-//const PERSONALITY_OF_BOT = "You talk like an old cowboy! You are a helpful chatbot from Teradata. As a crucial aspect of your function, ensure you always reference past user and assistant prompts in the thread for the best understanding in order to respond effectively.";
 
 class EchoBot extends ActivityHandler {
-	constructor(userState) {
-		super();
-		//DEBUG// console.log("\n\n**BOT_ROUTER.JS: EchoBot constructor has been called with userState: ", userState);
+  constructor(userState) {
+    super();
+    this.userState = userState;
+    this.botInvokedFlag = userState.createProperty('botInvokedFlag');
+    this.isFirstInteraction = userState.createProperty('isFirstInteraction');
+    this.userMessagesProperty = userState.createProperty(USER_MESSAGES);
+    this.membersAddedProperty = userState.createProperty(MEMBERS_ADDED);
+    this.onReactionsAdded(this.handleReactionForTeams.bind(this));
+    this.onMembersAdded(this.welcomeMembers.bind(this));
+    this.onMessage(this.processMessage.bind(this));
+  }
 
-		this.welcomedUserProperty = userState.createProperty(WELCOMED_USER);
-		this.chatMessagesProperty = userState.createProperty(CHAT_MESSAGES);
-		this.threadproperty = userState.createProperty(THREAD_TS);
-		this.botInvokedFlag = userState.createProperty('botInvokedFlag');
-		this.userState = userState;
-		// During bot initialization via msteams addition
-		this.isFirstInteraction = userState.createProperty('isFirstInteraction');
-		this.onReactionsAdded(this.handleMsTeamsReaction.bind(this));
+  async handleReactionForTeams(context) {
+    if (context.activity.channelId === 'msteams') {
+      const { reactionsAdded: [{ type: emoji }], from: { id: userId }, replyToId: messageId } = context.activity;
+      console.log(`User reacted to MS Teams post. UserId: ${userId}, Emoji: ${emoji}, MessageId: ${messageId}`);
+    }
+  }
 
+  async welcomeMembers(context, next) {
+    const pathConfig = PATH_CONFIGS[context.activity.channelId] || {};
+    const welcomeText = pathConfig.welcomeMessage || "Welcome!";
 
-		this.onMembersAdded(async (context, next) => {
-			console.log("\n\n**BOT_ROUTER.JS: A member(s) has been added to the chat");
-			console.log("\n\n**BOT_ROUTER.JS: The ids of the added members are: ", context.activity.membersAdded.map(member => member.id));
-			const membersAdded = context.activity.membersAdded;
+    const membersToAdd = context.activity.membersAdded;
+    for (let member of membersToAdd) {
+      if (member.id !== context.activity.recipient.id) {
+        await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+      }
+    }
+    await next();
+  }
 
-			// Get the path specific config here
-			const pathConfig = PATH_CONFIGS[context.activity.channelId];
-			const welcomeText = pathConfig ? pathConfig.welcomeMessage : "Hello and welcome to our chat!";
+  async processMessage(context, next) {
+    try {
+      const filename_ingress = "bot_router.js";
+      await botRouterSaveDataToPostgres(context.activity, context.activity.channelId, filename_ingress);
 
-			for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-				if (membersAdded[cnt].id !== context.activity.recipient.id) {
-					await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
-				}
-			}
-			await next();
-		});
+      const messageContent = context.activity.text.trim();
+      if (specialCommands[messageContent]) {
+        await specialCommands[messageContent](context, this.userMessagesProperty);
+      } else {
 
-		this.onMessage(async (context, next) => {
-			// save each payload to db regardless 2023oct12
-			try {
-				const filename_ingress = "bot_router.js";
-				await botRouterSaveDataToPostgres(context.activity, context.activity.channelId, filename_ingress);
-			} catch (err) {
-				console.error('\n\n**BOT_ROUTER.JS: Failed to save data to Postgres at botRouter path: ', err);
-			}
-			try {
-				const messageContent = context.activity.text.trim();
-		
-				console.log("\n\n**BOT_ROUTER.JS: Message content: ", context.activity.text);
-		
-				// Insert the following block here.
-				if (specialCommands[messageContent]) {
-					console.log("\n\n**BOT_ROUTER.JS: A special command has been detected.");
-					console.log("\n\n**BOT_ROUTER.JS: Command content: ", context.activity.text);
-		
-					// If the command exists in our special commands, execute it.
-					await specialCommands[messageContent](context, this.chatMessagesProperty);
-				} else {
-		
-					const pathConfig = PATH_CONFIGS[context.activity.channelId];
-					const personality = pathConfig ? pathConfig.personality : "Default personality";
-					let chatMessagesUser = await this.chatMessagesProperty.get(context, []) || [];
-					chatMessagesUser.push({
-						role: "user",
-						content: context.activity.text
-					});
-		
-					let isFirstInteraction = await this.isFirstInteraction.get(context, true);
-					let handled = false;
-		
-					handled = await handleMessageFromMSTeams(context, chatMessagesUser, isFirstInteraction, this.isFirstInteraction, PATH_CONFIGS['msteams']) || handled;		
-					if (handled) {
-						console.log("\n\n**BOT_ROUTER.JS: The handleMessageFromMSTeams function has successfully processed the incoming message. This means that the channel ID of the incoming message matched 'msteams'. The chat messages at this point are:", chatMessagesUser);
-						console.log("\n\n**BOT_ROUTER.JS: Next, this MSteams messages are being stored in the 'chatMessagesProperty' state property to determine next step.");
-						await this.chatMessagesProperty.set(context, chatMessagesUser);
-						return;
-					}
-		
-					handled = await handleMessageFromSlack(context, chatMessagesUser, this.threadproperty, this.botInvokedFlag, this.threadproperty, personality, PATH_CONFIGS['slack']);		
-					if (handled) {
-						console.log("\n\n**BOT_ROUTER.JS: The handleMessageFromSlack function has successfully processed the incoming message. This shows that the channel ID of the incoming message matched 'slack' and either the bot was summoned in the message text OR the bot was already interacting in the thread (its thread timestamp aligns with the current thread timestamp). So, the bot successfully processed the message from Slack. The chat messages at this point are:", chatMessagesUser);
-						console.log("\n\n**BOT_ROUTER.JS: Next, this Slack messages are being stored in the 'chatMessagesProperty' state property to determine next step.");
-						await this.chatMessagesProperty.set(context, chatMessagesUser);
-						return;
-					}
-		
-					handled = await handleDefault(context, chatMessagesUser, personality);		
-					if (handled) {
-						console.log("\n\n**BOT_ROUTER.JS: The message was handled by `handleDefault` because the triggers for msteams or slack were not met. The chat messages at this point are:", chatMessagesUser);
-						await this.chatMessagesProperty.set(context, chatMessagesUser);
-						await next();
-						return;
-					} 
-		
-					// After you save to postgres, call the next() function to continue to the next middleware in your pipeline.
-					await next();
-				}
-			} catch (error) {
-				console.error("\n\n**BOT_ROUTER.JS: An error occurred:", error);
-				if (error.type === 'content_filter') {
-					await context.sendActivity(error.message); // Send error message to user only for content_filter errors
-				}
-			}
-		});
-	}
-	async handleMsTeamsReaction(context) {
-		try {
-			if (context.activity.channelId === 'msteams') {
-				// Get the reaction added by the user
-				const userReaction = context.activity.reactionsAdded[0];
+        const pathConfig = PATH_CONFIGS[context.activity.channelId] || {};
+        const personality = pathConfig.personality || "Default personality";
 
-				// Extract information from the reaction
-				const userId = context.activity.from?.id;
-				const emoji = userReaction?.type;
-				const messageId = context.activity.replyToId;
+        let userMessages = await this.userMessagesProperty.get(context, []) || [];
+        userMessages.push({
+          role: "user",
+          content: context.activity.text
+        });
 
-				// Log the details to the console
-				console.log(`\n\n**BOT_ROUTER.JS: Someone reacted to a MSteams post! Here are the details:
-				\nThe userid is: ${userId}
-				\nThe emoji is: ${emoji}
-				\nThe messageID of the emoji is: ${messageId}`);
-			}
-		} catch (error) {
-			console.error('\n\n**BOT_ROUTER.JS:Failed to handle reaction:', error);
-		}
-	}
-	async run(context) {
-		//DEBUG console.log('\n\n**BOT_ROUTER.JS: Running the bot with context: ', context);
-		await super.run(context);
-		await this.userState.saveChanges(context);
-		//DEBUG console.log('\n\n**BOT_ROUTER.JS: Running the bot with context: ', context);
-	}
+        let isFirstInteraction = await this.isFirstInteraction.get(context, true);
+        const handled = await handleMessageFromMSTeams(context, userMessages, isFirstInteraction, this.isFirstInteraction, PATH_CONFIGS['msteams'])
+            || await handleMessageFromSlack(context, userMessages, this.threadproperty, this.botInvokedFlag, this.threadproperty, personality, PATH_CONFIGS['slack'])
+            || await handleDefault(context, userMessages, personality);
+
+        if (handled) {
+          await this.userMessagesProperty.set(context, userMessages);
+        }
+      }
+    } catch (error) {
+      if (error.type === 'content_filter') {
+        await context.sendActivity(error.message);
+      } else {
+        console.error("Error occurred:", error);
+      }
+    }
+
+    // Proceed to the next middleware
+    await next();
+  }
+
+  async run(context) {
+    await super.run(context);
+    await this.userState.saveChanges(context);
+  }
 }
 module.exports.EchoBot = EchoBot;
