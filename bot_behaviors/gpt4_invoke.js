90b1420d085f8ce49723e78f177abe37fe5ccdc5
Tillo <andytillo@gmail.com>
2023-10-30 11:23:10 -0700

    more logging

--- bot_behaviors/gpt4_invoke.js
+++ bot_behaviors/gpt4_invoke.js
@@ -8,32 +8,34 @@ axios.defaults.baseURL = OPENAI_API_BASE_URL;
 axios.defaults.headers['Content-Type'] = 'application/json';
 axios.defaults.headers['Authorization'] = `Bearer ${OPENAI_API_KEY}`;
 
-console.log(`Using the following OpenAI settings: ${OPENAI_API_KEY}, ${OPENAI_API_BASE_URL}, ${OPENAI_GPT4_ENGINE}.`);
+console.log('\n\n[DEBUG for gpt4_invoke.js]: Using the following OpenAI settings:', OPENAI_API_KEY, OPENAI_API_BASE_URL, OPENAI_GPT4_ENGINE);
 
 const invokeOpenaiGpt4 = async (prompt) => {
     const payload = {
         model: OPENAI_GPT4_ENGINE,
         messages: [
-            {"role": "system", "content": "You are a cutting edge AI."},
+            {"role": "system", "content": "You are an AI assistant."},
             {"role": "user", "content": prompt}
         ],
         max_tokens: 800,
         temperature: 0.7
     };
 
+    console.log('\n\n[DEBUG for gpt4_invoke.js]: Sending payload to OpenAI:', payload);
+
     try {
         const response = await axios.post(`/v1/engines/${OPENAI_GPT4_ENGINE}/completions`, payload);
-        console.log('OpenAI API Response: ', response.data);
+        console.log('\n\n[DEBUG for gpt4_invoke.js]: OpenAI API Response:', response.data);
         return response.data.choices[0].message.content;
     } catch (error) {
-        console.error('Error', error.message);
+        console.error('\n\n[DEBUG for gpt4_invoke.js]: Error', error.message);
         if (error.response) {
-            console.error('Error Response Data', error.response.data);
-            console.error('Error Response Status', error.response.status);
-            console.error('Error Response Headers', error.response.headers);
+            console.error('\n\n[DEBUG for gpt4_invoke.js]: Error Response Data', error.response.data);
+            console.error('\n\n[DEBUG for gpt4_invoke.js]: Error Response Status', error.response.status);
+            console.error('\n\n[DEBUG for gpt4_invoke.js]: Error Response Headers', error.response.headers);
         } else if (error.request) {
-            console.error('Error Request', error.request);
-        } 
+            console.error('\n\n[DEBUG for gpt4_invoke.js]: Error Request', error.request);
+        }
         return null;
     }
 };
