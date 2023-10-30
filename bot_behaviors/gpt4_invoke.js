fef58271a9ef54498e3c65c4b824a37251621776
Tillo <andytillo@gmail.com>
2023-10-30 11:14:23 -0700

    try to align/call gpt4 again

--- bot_behaviors/gpt4_invoke.js
+++ bot_behaviors/gpt4_invoke.js
@@ -4,50 +4,40 @@ const OPENAI_API_KEY = process.env['2023oct24_OPENAI_GPT4_API_KEY'];
 const OPENAI_API_BASE_URL = process.env['2023oct24_OPENAI_GPT4_API_BASE_URL'];
 const OPENAI_GPT4_ENGINE = process.env['2023oct24_OPENAI_GPT4_32K_API_ENGINE_DEPLOYMENT'];
 
-// Create an instance of axios with default settings
-const instance = axios.create({
-  baseURL: OPENAI_API_BASE_URL,
-  headers: {
-    'Authorization': `Bearer ${OPENAI_API_KEY}`,
-    'Content-Type': 'application/json'
-  }
-});
+axios.defaults.baseURL = OPENAI_API_BASE_URL;
+axios.defaults.headers['Content-Type'] = 'application/json';
+axios.defaults.headers['Authorization'] = `Bearer ${OPENAI_API_KEY}`;
 
-async function invokeOpenaiGpt4(prompt) {
-    console.log(`Using the following OpenAI settings: ${OPENAI_API_KEY}, ${OPENAI_API_BASE_URL}, ${OPENAI_GPT4_ENGINE}.`);
+console.log(`Using the following OpenAI settings: ${OPENAI_API_KEY}, ${OPENAI_API_BASE_URL}, ${OPENAI_GPT4_ENGINE}.`);
 
-    const body = {
+const invokeOpenaiGpt4 = async (prompt) => {
+    const payload = {
         model: OPENAI_GPT4_ENGINE,
-        messages: [{
-            "role": "system",
-            "content": "You are an AI assistant that helps people find information."
-        }, {
-            "role": "user",
-            "content": prompt
-        }],
+        messages: [
+            {"role": "system", "content": "You are a cutting edge AI."},
+            {"role": "user", "content": prompt}
+        ],
         max_tokens: 800,
-        temperature: 0.9
+        temperature: 0.7
     };
 
-    console.log(`Hitting OpenAI API with URL: ${OPENAI_API_BASE_URL}/v1/engines/${OPENAI_GPT4_ENGINE}/completions and body:`, body);
-    
     try {
-        const response = await instance.post(`/v1/engines/${OPENAI_GPT4_ENGINE}/completions`, body);
+        const response = await axios.post(`/v1/engines/${OPENAI_GPT4_ENGINE}/completions`, payload);
         console.log('OpenAI API Response: ', response.data);
         return response.data.choices[0].message.content;
     } catch (error) {
-        console.error(`Failed to call the OpenAI API: ${error.message}`);
+        console.error('Error', error.message);
         if (error.response) {
-            console.error(error.response.data);
-            console.error(error.response.status);
-            console.error(error.response.headers);
+            console.error('Error Response Data', error.response.data);
+            console.error('Error Response Status', error.response.status);
+            console.error('Error Response Headers', error.response.headers);
         } else if (error.request) {
-            console.error(error.request);
+            console.error('Error Request', error.request);
         } 
         return null;
     }
-}
+};
 
 module.exports = {
     invokeOpenaiGpt4
-}
+};
