90bad7c23fbe7744875f6d03c3aa27697be70b79
Tillo <andytillo@gmail.com>
2023-11-01 17:33:12 -0700

    273a

--- utilities/tree_nation/public_api_endpoints.js
+++ utilities/tree_nation/public_api_endpoints.js
@@ -1,63 +1,67 @@
 const axios = require('axios');
 
-class Summary{
-  constructor(){
-    this.totalProjects = 0;
-    this.activeProjects = 0;
-    this.inactiveProjects = 0;
-    this.averageCo2 = 0;
-    this.averageTreeStock = 0;
-    this.uniqueLocations = [];
-  }
+class Summary {
+	constructor() {
+		this.totalProjects = 0;
+		this.activeProjects = 0;
+		this.inactiveProjects = 0;
+		this.averageCo2 = 0;
+		this.averageTreeStock = 0;
+		this.uniqueLocations = [];
+	}
 
-  createSummary(){
-    return {
-      totalProjects: this.totalProjects,
-      activeProjects: this.activeProjects,
-      inactiveProjects: this.inactiveProjects,
-      averageCo2: this.averageCo2,
-      averageTreeStock: this.averageTreeStock,
-      uniqueLocations: this.uniqueLocations,
-    };
-  }
+	createSummary() {
+		return {
+			totalProjects: this.totalProjects,
+			activeProjects: this.activeProjects,
+			inactiveProjects: this.inactiveProjects,
+			averageCo2: this.averageCo2,
+			averageTreeStock: this.averageTreeStock,
+			uniqueLocations: this.uniqueLocations,
+		};
+	}
 }
 
 async function getTreeNationProjectsSummary() {
-  const summary = new Summary();
-  const projects_url = 'https://tree-nation.com/api/projects';
+	const summary = new Summary();
+	const projects_url = 'https://tree-nation.com/api/projects';
 
-  try {
-    let response = await axios.get(projects_url);
-    let projects = response.data;
-    console.log(`\n\n**TREE-NATION-PUBLIC_API_ENDPOINTS: Total Number of Projects: ${projects.length}`);
+	try {
+		let response = await axios.get(projects_url);
+		let projects = response.data;
+		console.log(`\n\n**TREE-NATION-PUBLIC_API_ENDPOINTS: Total Number of Projects: ${projects.length}`);
 
-    summary.totalProjects = projects.length;
+		summary.totalProjects = projects.length;
 
-    let activeProjects = projects.filter( project => project.status === 'active');
-    summary.activeProjects = activeProjects.length;
+		let activeProjects = projects.filter(project => project.status === 'active');
+		summary.activeProjects = activeProjects.length;
 
-    let inactiveProjects = projects.filter( project => project.status === 'inactive');
-    summary.inactiveProjects = inactiveProjects.length;
+		let inactiveProjects = projects.filter(project => project.status === 'inactive');
+		summary.inactiveProjects = inactiveProjects.length;
 
-    let totalCo2 = 0;
-    let totalStock = 0;
-    let locations = new Set();
+		let totalCo2 = 0;
+		let totalStock = 0;
+		let locations = new Set();
 
-    for(let project of projects){
-        try{
-            totalCo2 += project.co2_compensated_tons || 0; 
-            totalStock += project.stock || 0;
-            locations.add(project.location);
-        }catch (error) {
-            throw new Error(`Error processing project: ${project.id}, error detail: ${error.message}`);
+        for (let project of projects) {
+            try {
+                if(typeof project.co2_compensated_tons !== "undefined"){
+                    totalCo2 += project.co2_compensated_tons;
+                }
+                if(typeof project.stock !== "undefined"){
+                    totalStock += project.stock;
+                }
+                    locations.add(project.location);
+            } catch (error) {
+                throw new Error(`Error processing project: ${project.id}, error detail: ${error.message}`);
+            }
         }
-    }
 
-    summary.averageCo2 = totalCo2 / projects.length;
-    summary.averageTreeStock = totalStock / projects.length;
-    summary.uniqueLocations = Array.from(locations);
+		summary.averageCo2 = totalCo2 / projects.length;
+		summary.averageTreeStock = totalStock / projects.length;
+		summary.uniqueLocations = Array.from(locations);
 
-    let resultText = `
+		let resultText = `
     Total Projects: ${result.totalProjects}
     Active Projects: ${result.activeProjects}
     Inactive Projects: ${result.inactiveProjects}
@@ -66,25 +70,25 @@ async function getTreeNationProjectsSummary() {
     Unique Locations: ${result.uniqueLocations.join(', ')}
   `;
 
-  return resultText; 
+		return resultText;
 
-  } catch (error) {
-      console.error(error);
-      return 'An error occurred while fetching Tree Nation projects data.';
-  }
+	} catch (error) {
+		console.error(error);
+		return 'An error occurred while fetching Tree Nation projects data.';
+	}
 }
 
 async function getData() {
-    try {
-        const summary = await getTreeNationProjectsSummary();
-        console.log(summary);
-    } catch (err) {
-        console.log('An error occurred during the data fetching or processing.', err);
-    }
+	try {
+		const summary = await getTreeNationProjectsSummary();
+		console.log(summary);
+	} catch (err) {
+		console.log('An error occurred during the data fetching or processing.', err);
+	}
 }
 
 getData();
 
 module.exports = {
-    getTreeNationProjectsSummary,
+	getTreeNationProjectsSummary,
 };
