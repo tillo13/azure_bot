ee21e49e66b9682829727c5736e1c0c70a340b04
Tillo <andytillo@gmail.com>
2023-11-01 23:03:11 -0700

    not worth it

--- bot_behaviors/weaviate_utils.js
+++ bot_behaviors/weaviate_utils.js
@@ -1,155 +1,249 @@
 //2023nov1 add in random_obj finder
 const fetch = require('node-fetch');
-const { invokeOpenaiGpt4 } = require('./gpt4_invoke');
+
 const { COSINE_SIMILARITY_THRESHOLD } = require('../utilities/global_configs');
 
-const CONFIGS = {
-    BASE_URL: process.env['2023oct25_WEAVIATE_URL'],
-    CLASS_NAME: process.env['2023oct25_WEAVIATE_CLASS_NAME'],
-    OBJECT_VALUE: process.env['2023oct25_WEAVIATE_CLASS_OBJ_VALUE'],
-    AUTH_TOKEN: `Bearer ${process.env["2023oct21_WEAVIATE_API_KEY"]}`,
-    AZURE_API_KEY: `${process.env["2023oct21_AZURE_OPENAI_API_KEY"]}`,
-    COSINE_SIMILARITY_THRESHOLD,
-    UNWANTED_TERM: "zzzzz",
-    MOVE_AWAY_FORCE: 0.0,
-    LIMIT: 3
-};
+const { invokeOpenaiGpt4 } = require('./gpt4_invoke');
+
+const path = require('path');
+
+const base_url = process.env['2023oct25_WEAVIATE_URL'];
+const weaviate_class_name = process.env['2023oct25_WEAVIATE_CLASS_NAME'];
+const weaviate_object_value = process.env['2023oct25_WEAVIATE_CLASS_OBJ_VALUE'];
+const url = `${base_url}/v1/graphql`;
 
 const headers = {
     'Content-Type': 'application/json',
-    'Authorization': CONFIGS.AUTH_TOKEN,
-    'X-Azure-Api-Key': CONFIGS.AZURE_API_KEY
-};
-
-const url = `${CONFIGS.BASE_URL}/v1/graphql`;
-
-async function queryWeaviate(query) {
-    const response = await fetch(url, { method: 'POST', headers, body: JSON.stringify(query) });
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return await response.json();
+    'Authorization': `Bearer ${process.env["2023oct21_WEAVIATE_API_KEY"]}`,
+    'X-Azure-Api-Key': `${process.env["2023oct21_AZURE_OPENAI_API_KEY"]}`,
 }
 
-async function fetchVectorsWithSimilarity(searchTerm) {
-    return await queryWeaviate({
-        query: `{
-            Get {
-                ${CONFIGS.CLASS_NAME}(nearText: {
-                    concepts: ["${searchTerm}"],
-                    certainty: ${CONFIGS.SIMILARITY_THRESHOLD},
-                    moveAwayFrom: {concepts: ["${CONFIGS.UNWANTED_TERM}"], force: ${CONFIGS.MOVE_AWAY_FORCE}}
-                }, limit: ${CONFIGS.LIMIT}) {
-                    ${CONFIGS.OBJECT_VALUE}
-                    _additional {certainty}
+const CLASS_NAME = weaviate_class_name;
+const className = weaviate_class_name;
+const SIMILARITY_THRESHOLD = 0.7;
+const OBJECT_VALUE = weaviate_object_value;
+const LIMIT = 3;
+
+const UNWANTED_TERM = "zzzzz";
+const MOVE_AWAY_FORCE = 0.0;
+
+async function initialSearchVectorSimilarity(searchTerm) {
+    try {
+        const payload = {
+            query: `
+            {
+                Get {
+                    ${CLASS_NAME}(nearText: {
+                        concepts: ["${searchTerm}"],
+                        certainty: ${SIMILARITY_THRESHOLD},
+                        moveAwayFrom: {concepts: ["${UNWANTED_TERM}"], force: ${MOVE_AWAY_FORCE}}
+                    }, limit: ${LIMIT}) {
+                        ${OBJECT_VALUE}
+                        _additional {certainty}
+                    }
                 }
             }
-        }`
-    });
+            `
+        };
+        const response = await fetch(url, { method: 'POST', headers: headers, body: JSON.stringify(payload) });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        //console.log("Similarity Search:");
+        //console.log(JSON.stringify(data, null, 4));
+        //console.log();
+
+        return data;
+    } catch(e) {
+        console.log(`\n\n******WEAVIATE_UTILS.JS: There was a problem with the fetch operation: ${e.message}`);
+    }
 }
 
 async function handleSearchSimilarity(lastUserMessage){
     console.log(`\n\n******WEAVIATE_UTILS.JS: Message we will pass to Weaviate: ${lastUserMessage}`);
     
-    const weaviateResponse = await fetchVectorsWithSimilarity(lastUserMessage);
+    const weaviateResponse = await initialSearchVectorSimilarity(lastUserMessage);
     if (weaviateResponse?.data?.Get) {
+        // get the class name
         let className = Object.keys(weaviateResponse.data.Get)[0];
+
+        // wrap the object in array if it's an object
         let responseData = Array.isArray(weaviateResponse.data.Get[className]) 
             ? weaviateResponse.data.Get[className] 
             : [weaviateResponse.data.Get[className]];
 
-        let cosines = responseData.map((obj, i) => {
+        // Initialize array to hold cosines
+        let cosines = [];
+
+        responseData.forEach((obj, i) => {
             console.log(`\n\n******WEAVIATE_UTILS.JS: Weaviate similarity cosine #${i + 1} : ${obj._additional.certainty}`);
             console.log(`\n\n******WEAVIATE_UTILS.JS: Weaviate response #${i + 1} : ${JSON.stringify(obj[OBJECT_VALUE])}`);
-            return obj._additional.certainty;
+
+            // Store cosine similarity
+            cosines.push(obj._additional.certainty);
         });
-        let highestScore = cosines.length > 0 ? Math.max.apply(Math, cosines) : 0;
+        //return {className: className, data: responseData, cosines: cosines};
+        //add the highest cosine similarity score to the return object
+        return {className: className, data: responseData, cosines: cosines, highestScore: Math.max(...cosines)};
 
-        return { className, data: responseData, cosines, highestScore};
     } else {
-        console.log("\n\n******WEAVIATE_UTILS.JS: Unable to communicate with Weaviate");
-        return { highestScore: 0 };
-    }
-}
-
-async function enhanceResponseWithWeaviate(lastUserMessage, chatMessagesAfterExtraction, weaviateResponse) {
-    let {weaviateInfo, countAboveThreshold} = await formatWeaviateResponse(weaviateResponse);
-    if (countAboveThreshold > 0) {
-        let gpt4Prompt = `A user provided this statement: ${lastUserMessage}. We found ${countAboveThreshold} matches in our Teradata-specific vector dataset with cosine similarity of ${CONFIGS.COSINE_SIMILARITY_THRESHOLD} or higher that we deem suitable in a response. Please read this, and respond back cleanly to the user using this as your primary source of data, feel free to enhance it if you know more about the subject, but do not hallucinate. ${weaviateInfo}.`;
-        return await invokeOpenaiGpt4(gpt4Prompt);
-    } else {
-        console.log("\n\n******WEAVIATE_UTILS.JS: No high cosine similarity score was found, therefore not enhancing with Weaviate nor GPT4 for speed/finance reasons.");
+        console.log("\n\n******WEAVIATE_UTILS.JS: Could not communicate with Weaviate");
         return null;
     }
 }
 
+async function enhanceResponseWithWeaviate(lastUserMessage, chatMessagesAfterExtraction, weaviateResponse) {
+	try {
+		// Obtain the formatted Weaviate information and count of high similarity matches
+		let {
+			weaviateInfo,
+			countAboveThreshold
+		} = await formatWeaviateResponse(weaviateResponse);
+		console.log(`\n\nNumber of matches above threshold via chat_helper.js: ${countAboveThreshold}`);
+		// Ensure countAboveThreshold is a number
+		countAboveThreshold = Number(countAboveThreshold);
+		// If weaviateInfo is undefined, set it to an empty string
+		weaviateInfo = weaviateInfo || "";
+
+		// Continue only if there are matches above the similarity threshold
+		if (countAboveThreshold > 0) {
+			let gpt4Prompt = `A user provided this statement: ${lastUserMessage}. We found ${countAboveThreshold} matches in our Teradata-specific vector dataset with cosine similarity of ${COSINE_SIMILARITY_THRESHOLD} or higher that we deem suitable in a response. Please read this, and respond back cleanly to the user using this as your primary source of data, feel free to enhance it if you know more about the subject, but do not hallucinate. ${weaviateInfo}.`;
+			// Now use 'gpt4Prompt' to invoke GPT4
+			return await invokeOpenaiGpt4(gpt4Prompt);
+            assistantResponse = gpt4Prompt
+		} else {
+			console.log("\n\n***CHAT_HELPER.JS: No high cosine similarity score was found, therefore not enhancing with Weaviate nor GPT4 for speed/finance reasons.");
+            //GPT3.5 is perfectly capable of responding for speed, if we want to use GPT4 in the future for non-cosine, uncomment these lines to invoke GPT4
+			//let gpt4Prompt = `A user provided this statement: ${lastUserMessage}. Please provide a thorough response using any knowledge you have, but do not hallucinate.`;
+			//return await invokeOpenaiGpt4(gpt4Prompt);
+		}
+	} catch (err) {
+		// In case of error, log it
+		console.log("\n\n***CHAT_HELPER.JS: Error occurred while enhancing with Weaviate: ", err);
+		// Return null on failing to enhance the response
+		return null;
+	}
+}
+
 function formatWeaviateResponse(weaviateResponse) {
     let weaviateInfo = "";
+
+    // Initialize counter
     let countAboveThreshold = 0;
 
-    const cosines = weaviateResponse?.cosines || [];
-    const data = weaviateResponse?.data || [];
-
-    if (data.length > 0 && cosines.length > 0) {
+    if(weaviateResponse && weaviateResponse.data.length > 0 && weaviateResponse.cosines.length > 0) {
         let hasValidResult = false;
         let tempInfo = "\n\nWeaviate Results:\n";
 
-        for (let index = 0; index < data.length; index++) {
-            if(cosines[index] >= COSINE_SIMILARITY_THRESHOLD) {
-                tempInfo += `Result ${index + 1}: ${JSON.stringify(data[index])}\n`;
-                tempInfo += `Cosine Similarity: ${cosines[index]}\n`;
+        for(let index in weaviateResponse.data) {
+            
+            if(weaviateResponse.cosines[index] >= COSINE_SIMILARITY_THRESHOLD) {
+                tempInfo += `Result ${index + 1}: ${JSON.stringify(weaviateResponse.data[index])}\n`;
+                tempInfo += `Cosine Similarity: ${weaviateResponse.cosines[index]}\n`;
 
+                // Increment the counter
                 countAboveThreshold++;
+
+                // Set flag that there's a valid result
                 hasValidResult = true;
             } else {
-                console.log(`\n\n***CHAT_HELPER.JS: Result ${index + 1} has a cosine similarity of ${cosines[index]}, which is below the desired threshold. Not displaying it to the user.`);
+                console.log(`\n\n***CHAT_HELPER.JS: Result ${index + 1} has a cosine similarity of ${weaviateResponse.cosines[index]}, which is below the desired threshold. Not displaying it to the user.`);
             }
         }
 
-        if(hasValidResult) {
+        // Only assign tempInfo to weaviateInfo if there's a valid result
+        if (hasValidResult) {
             weaviateInfo = tempInfo;
         }
     }
+
     console.log(`\n\nNumber of matches above threshold via weaviate_utils.js: ${countAboveThreshold}`);
-    return {weaviateInfo, countAboveThreshold};
+
+    return {weaviateInfo: weaviateInfo, countAboveThreshold: countAboveThreshold};
 }
 
 async function getRandomObject() {
     const MAX_RETRIES = 3;
+    let message = "";
+
     for(let i = 0; i < MAX_RETRIES; i++){
         try {
-            const result = await queryWeaviate({ query: `{ Aggregate { ${CONFIGS.CLASS_NAME} { meta { count } } } }` });
-            const totalObjects = result?.data?.Aggregate?.[CONFIGS.CLASS_NAME]?.[0]?.meta?.count || 0;
+            let query = {
+                query: `{
+                    Aggregate {
+                        ${className} {
+                            meta {
+                                count
+                            }
+                        }
+                    }
+                }`
+            };
+    
+            let response = await fetch(url, { method: 'POST', headers: headers, body: JSON.stringify(query) });
+            
+            if (!response.ok) {
+                console.error(`HTTP error, unable to fetch! status: ${response.status}`);
+            }
+
+            let result = await response.json();
+
+            const totalObjects = result && result.data && result.data.Aggregate &&
+                                 result.data.Aggregate[className] && result.data.Aggregate[className][0] &&
+                                 result.data.Aggregate[className][0].meta && result.data.Aggregate[className][0].meta.count;
+            
             if (totalObjects > 0) {
                 const randomOffset = Math.floor(Math.random() * totalObjects);
-                const result2 = await queryWeaviate({
-                    query: `{ Get { ${CONFIGS.CLASS_NAME}(limit: 1, offset: ${randomOffset}) { _additional { id } ${CONFIGS.OBJECT_VALUE} } } }`
-                });
-                const randomId = result2?.data?.Get?.[CONFIGS.CLASS_NAME]?.[0]?._additional?.id;
-                const randomObject = result2?.data?.Get?.[CONFIGS.CLASS_NAME]?.[0]?.[CONFIGS.OBJECT_VALUE];
+                query = {
+                    query: `{
+                        Get {
+                            ${className}(limit: 1, offset: ${randomOffset}) {
+                                _additional {
+                                    id
+                                }
+                                ${OBJECT_VALUE}
+                            }
+                        }
+                    }`
+                };
+                response = await fetch(url, { method: 'POST', headers: headers, body: JSON.stringify(query) });
+
+                if (!response.ok) {
+                    console.error(`HTTP error, unable to fetch! status: ${response.status}`);
+                }
+
+                result = await response.json();
+
+                const randomId = result && result.data && result.data.Get && 
+                                result.data.Get[className] && result.data.Get[className][0] && 
+                                result.data.Get[className][0]._additional && result.data.Get[className][0]._additional.id;
+
+                const randomObject = result && result.data && result.data.Get && 
+                                    result.data.Get[className] && result.data.Get[className][0] && 
+                                    result.data.Get[className][0][OBJECT_VALUE];
+
                 if(randomId && randomObject){
-                    return JSON.stringify(result2.data.Get[CONFIGS.CLASS_NAME][0], null, 2);
+                    message = JSON.stringify(result.data.Get[className][0], null, 2);
                 } else {
                     throw new Error(`Failed to fetch data for id and object at offset: ${randomOffset}`);
                 }
             } else {
-                console.error(`No object found for the class: ${CONFIGS.CLASS_NAME}`);
-                return `No object found for the class: ${CONFIGS.CLASS_NAME}`;
+                console.error(`No object found for the class: ${className}`);
+                message = `No object found for the class: ${className}`;
             }
+            break;
         } catch (e) {
             console.error(`An error occurred: ${e.message}`);
             if(i === MAX_RETRIES - 1) {
                 console.error('Unable to get random object after maximum retries. Please try again later.');
-                return 'Unable to get random object after maximum retries. Please try again later.';
+                message = 'Unable to get random object after maximum retries. Please try again later.';
             }
         }
     }
+
+    console.log(message);
+    return message;  
 }
 
-module.exports = {
-    initialSearchVectorSimilarity: fetchVectorsWithSimilarity,
-    handleSearchSimilarity: handleSearchSimilarity,
-    formatWeaviateResponse: formatWeaviateResponse,
-    enhanceResponseWithWeaviate: enhanceResponseWithWeaviate,
-    getRandomObject: getRandomObject
-};
+module.exports = {initialSearchVectorSimilarity, handleSearchSimilarity, formatWeaviateResponse, enhanceResponseWithWeaviate, getRandomObject}
