6b36204d9a1bce0363311be77456b5a2c777f623
Tillo <andytillo@gmail.com>
2023-11-20 12:40:37 -0800

    send env for test

--- utilities/tree_nation/tree_nation_utils.js
+++ utilities/tree_nation/tree_nation_utils.js
@@ -17,6 +17,7 @@ const config = {
   };
   
   async function plantTree(environmentFlag, recipients, quantity, message, maxRetries = 3, retryDelayMs = 2000) {
+
     const environment = config[environmentFlag];
     const apiUrl = `${environment.site}/api/plant`;
     const headers = {
@@ -27,6 +28,7 @@ const config = {
     // Get the species id
     const projectIds = environmentFlag === 'TEST' ? [3, 41] : []; // Define your production project IDs here if any
     const species = await getRandomAvailableSpecies(projectIds);
+    
     const payload = {
       recipients: recipients,
       planter_id: environment.planterId,
@@ -41,13 +43,13 @@ const config = {
       try {
         const response = await axios.post(apiUrl, payload, { headers: headers });
         if (response.data.status === 'ok') {
-            // Success
-            return {
-              userMessage: createDetailResponse(response.data),
-              consoleMessage: createVerboseConsoleLog(response.data),
-              status: 'ok',
-              data: response.data
-            };
+          // Pass both species and environmentFlag to createDetailResponse
+          return {
+            userMessage: createDetailResponse(response.data, species, environmentFlag),
+            consoleMessage: createVerboseConsoleLog(response.data),
+            status: 'ok',
+            data: response.data
+          };
         } else {
           // Handle non-success status here without retrying
           return {
@@ -71,24 +73,6 @@ const config = {
     }
   }
 
-function createDetailResponse(apiResponse) {
-    let treeDetails = apiResponse.trees.map(tree => {
-        return `Tree ID: ${tree.id}\n` +
-               `Token: ${tree.token}\n` +
-               `Collect URL: ${tree.collect_url}\n` +
-               `Certificate URL: ${tree.certificate_url}\n`;
-    }).join('\n\n');
-
-    return `A tree has been planted successfully! Here are the details:\n\n${treeDetails}`;
-}
-
-function createVerboseConsoleLog(apiResponse) {
-    let treeDetails = apiResponse.trees.map(tree => {
-        return `Tree ID: ${tree.id}, Token: ${tree.token}, Collect URL: ${tree.collect_url}, Certificate URL: ${tree.certificate_url}`;
-    }).join('; ');
-
-    return `\n***TREE_NATION_UTILS.JS: **Tree Planting Request Successful**\nPayment ID: ${apiResponse.payment_id}\nStatus: ${apiResponse.status}\nTrees Details: ${treeDetails}`;
-}
 
 async function getAvailableSpecies(projectId) {
     const apiUrl = `https://tree-nation.com/api/projects/${projectId}/species`;
@@ -112,14 +96,14 @@ async function getAvailableSpecies(projectId) {
   
       for (const projectId of projectIds) {
         const availableSpecies = await getAvailableSpecies(projectId);
-        allAvailableSpecies.push(...availableSpecies);
+        allAvailableSpecies.push(...availableSpecies.map(species => ({ ...species, projectId }))); // Attach projectId to each species
       }
   
       if (allAvailableSpecies.length === 0) {
         throw new Error('No species with positive stock are available for planting.');
       }
   
-      // Randomly pick an available species
+      // Randomly pick an available species and its associated project
       const randomIndex = Math.floor(Math.random() * allAvailableSpecies.length);
       return allAvailableSpecies[randomIndex];
     } catch (error) {
@@ -127,6 +111,32 @@ async function getAvailableSpecies(projectId) {
       throw error;
     }
   }
+
+  function createDetailResponse(apiResponse, speciesObject, environmentFlag) { // Now also takes environmentFlag
+    let treeDetails = apiResponse.trees.map(tree => {
+        return `Tree ID: ${tree.id}\n` +
+               `Token: ${tree.token}\n` +
+               `Collect URL: ${tree.collect_url}\n` +
+               `Certificate URL: ${tree.certificate_url}\n`;
+    }).join('\n\n');
+  
+    let projectName = 'the chosen location';
+    if (environmentFlag === 'TEST') {
+      projectName = speciesObject.projectId === 3 ? 'La Pedregoza' :
+                    speciesObject.projectId === 41 ? 'Usambara Biodiversity Conservation' :
+                    projectName;
+    }
+  
+    return `A tree has been planted successfully${environmentFlag === 'TEST' ? ` in <b>${projectName}</b>` : ''}! Here are the details:\n\n${treeDetails}`;
+  }
+
+function createVerboseConsoleLog(apiResponse) {
+    let treeDetails = apiResponse.trees.map(tree => {
+        return `Tree ID: ${tree.id}, Token: ${tree.token}, Collect URL: ${tree.collect_url}, Certificate URL: ${tree.certificate_url}`;
+    }).join('; ');
+
+    return `\n***TREE_NATION_UTILS.JS: **Tree Planting Request Successful**\nPayment ID: ${apiResponse.payment_id}\nStatus: ${apiResponse.status}\nTrees Details: ${treeDetails}`;
+}
 
 module.exports = {
     plantTree
