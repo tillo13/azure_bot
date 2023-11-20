c44c6caf331f68a1fa3c0cf4caa94c1fa965a418
Tillo <andytillo@gmail.com>
2023-11-20 11:26:53 -0800

    set env vars for ONLY test to use specific projects per tn admin and set retries

--- utilities/tree_nation/tree_nation_utils.js
+++ utilities/tree_nation/tree_nation_utils.js
@@ -1,5 +1,8 @@
 const axios = require('axios');
 
+// Helper function for sleep
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
 const config = {
     TEST: {
       site: process.env['2023nov16_TREE_NATION_TEST_SITE'],
@@ -41,52 +44,59 @@ const config = {
     return allAvailableSpecies[randomIndex];
   }
   
-  async function plantTree(environmentFlag, recipients, quantity, message) {
+
+  async function plantTree(environmentFlag, recipients, quantity, message, maxRetries = 3, retryDelayMs = 2000) {
     const environment = config[environmentFlag];
     const apiUrl = `${environment.site}/api/plant`;
     const headers = {
-        'Authorization': `Bearer ${environment.token}`,
-        'Content-Type': 'application/json'
+      'Authorization': `Bearer ${environment.token}`,
+      'Content-Type': 'application/json'
     };
   
-    // Randomly select a valid species for planting
-    // Determine the project IDs based on the environment
+    // Get the species id
     const projectIds = environmentFlag === 'TEST' ? [3, 41] : []; // Define your production project IDs here if any
-
     const species = await getRandomAvailableSpecies(projectIds);
-    
     const payload = {
       recipients: recipients,
       planter_id: environment.planterId,
-      species_id: species.id, // Use the species ID from the available species
+      species_id: species.id,
       quantity: quantity,
       message: message
     };
   
-    try {
-      const response = await axios.post(apiUrl, payload, { headers: headers });
-      if (response.data.status === 'ok') {
-        return {
-          userMessage: createDetailResponse(response.data),
-          consoleMessage: createVerboseConsoleLog(response.data),
-          status: 'ok',
-          data: response.data // This should contain the trees array in its structure
-        };
-      } else {
-        // Error occurred on the Tree-Nation API side
-        return {
-          userMessage: `An error occurred while planting the tree: ${response.data.message}`,
-          consoleMessage: `\n***TREE_NATION_UTILS.JS: Tree planting request failed with status: ${response.data.status}`,
-          status: 'error'
-        };
+    let attempt = 0;
+  
+    while (attempt < maxRetries) {
+      try {
+        const response = await axios.post(apiUrl, payload, { headers: headers });
+        if (response.data.status === 'ok') {
+            // Success
+            return {
+              userMessage: createDetailResponse(response.data),
+              consoleMessage: createVerboseConsoleLog(response.data),
+              status: 'ok',
+              data: response.data
+            };
+        } else {
+          // Handle non-success status here without retrying
+          return {
+            userMessage: `An error occurred while planting the tree: ${response.data.message}`,
+            consoleMessage: `\n***TREE_NATION_UTILS.JS: Tree planting request failed with status: ${response.data.status}`,
+            status: 'error'
+          };
+        }
+      } catch (error) {
+        console.error(`Attempt ${attempt + 1} encountered an error: ${error.message}`);
+        attempt++; // We only increase the attempt count when an error occurs that we might want to retry
+  
+        if (attempt < maxRetries) {
+          console.log(`Retrying attempt ${attempt} after ${retryDelayMs} ms...`);
+          await sleep(retryDelayMs);
+        } else {
+          // All retries have failed, throw the error so it can be handled higher up or logged
+          throw new Error(`Failed to plant tree after ${maxRetries} attempts: ${error}`);
+        }
       }
-    } catch (error) {
-      // Error occurred while making HTTP request
-      return {
-        userMessage: `An error occurred while connecting to the Tree-Nation API: ${error.message}`,
-        consoleMessage: `\n***TREE_NATION_UTILS.JS: Error while making the plant tree HTTP request: ${error.message}`,
-        status: 'error'
-      };
     }
   }
 
@@ -109,44 +119,44 @@ function createVerboseConsoleLog(apiResponse) {
     return `\n***TREE_NATION_UTILS.JS: **Tree Planting Request Successful**\nPayment ID: ${apiResponse.payment_id}\nStatus: ${apiResponse.status}\nTrees Details: ${treeDetails}`;
 }
 
+async function getAvailableSpecies(projectId) {
+    const apiUrl = `https://tree-nation.com/api/projects/${projectId}/species`;
+    try {
+      const response = await axios.get(apiUrl);
+      if (response.status === 200) {
+        // Filter species to get only those with stock > 0
+        return response.data.filter(species => species.stock > 0);
+      } else {
+        throw new Error(`Could not retrieve species for project ID ${projectId}`);
+      }
+    } catch (error) {
+      console.error(error.message);
+      throw error;
+    }
+  }
+  
+  async function getRandomAvailableSpecies(projectIds) {
+    try {
+      const allAvailableSpecies = [];
+  
+      for (const projectId of projectIds) {
+        const availableSpecies = await getAvailableSpecies(projectId);
+        allAvailableSpecies.push(...availableSpecies);
+      }
+  
+      if (allAvailableSpecies.length === 0) {
+        throw new Error('No species with positive stock are available for planting.');
+      }
+  
+      // Randomly pick an available species
+      const randomIndex = Math.floor(Math.random() * allAvailableSpecies.length);
+      return allAvailableSpecies[randomIndex];
+    } catch (error) {
+      console.error(error.message);
+      throw error;
+    }
+  }
+
 module.exports = {
     plantTree
 };
-
-async function getAvailableSpecies(projectId) {
-  const apiUrl = `https://tree-nation.com/api/projects/${projectId}/species`;
-  try {
-    const response = await axios.get(apiUrl);
-    if (response.status === 200) {
-      // Filter species to get only those with stock > 0
-      return response.data.filter(species => species.stock > 0);
-    } else {
-      throw new Error(`Could not retrieve species for project ID ${projectId}`);
-    }
-  } catch (error) {
-    console.error(error.message);
-    throw error;
-  }
-}
-
-async function getRandomAvailableSpecies(projectIds) {
-  try {
-    const allAvailableSpecies = [];
-
-    for (const projectId of projectIds) {
-      const availableSpecies = await getAvailableSpecies(projectId);
-      allAvailableSpecies.push(...availableSpecies);
-    }
-
-    if (allAvailableSpecies.length === 0) {
-      throw new Error('No species with positive stock are available for planting.');
-    }
-
-    // Randomly pick an available species
-    const randomIndex = Math.floor(Math.random() * allAvailableSpecies.length);
-    return allAvailableSpecies[randomIndex];
-  } catch (error) {
-    console.error(error.message);
-    throw error;
-  }
-}
